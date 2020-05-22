5e1ebe9b55704dd9ce24013b5adb53ae6a4fc0f4
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-05-22 22:18:13 +0200

    Implement input value validation

--- WebApps/ShoppingList/ShoppingList/EntityModels/ItemDto.cs
+++ WebApps/ShoppingList/ShoppingList/EntityModels/ItemDto.cs
@@ -1,18 +1,26 @@
-﻿namespace ShoppingList.EntityModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.EntityModels
 {
     public class ItemDto
     {
         public uint Id { get; set; }
         public uint? ItemCategoryId { get; set; }
         public uint? ManufacturerId { get; set; }
+
+        [MaxLength(255, ErrorMessage = "The name mustn't be longer than 255 characters")]
         public string Name { get; set; }
         public uint Quantity { get; set; }
         public bool IsInShoppingBasket { get; set; }
         public float QuantityInPacket { get; set; }
+
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ConvertValueInInvariantCulture = true, ErrorMessage = "Price mustn't be negative")]
         public decimal PricePerQuantity { get; set; }
         public bool Active { get; set; }
         public uint StoreId { get; set; }
         public QuantityType QuantityType { get; set; }
+
+        [MaxLength(255, ErrorMessage = "The comment mustn't be longer than 255 characters")]
         public string Comment { get; set; }
         public QuantityType QuantityInPacketType { get; set; }
     }
