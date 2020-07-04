ac7e0b931a5f95cec51ae42eebd9640690179a74
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-07-04 12:00:24 +0200

    Implement adding temporary items to shopping list

--- WebApps/ShoppingList/ShoppingList/Mapper/Mapper.cs
+++ WebApps/ShoppingList/ShoppingList/Mapper/Mapper.cs
@@ -21,7 +21,8 @@ namespace ShoppingList.Mapper
                 StoreId = item.StoreId,
                 Active = item.Active.Value,
                 Comment = item.Comment,
-                QuantityInPacketType = ToQuantityType(item.QuantityInPacketTypeId)
+                QuantityInPacketType = ToQuantityType(item.QuantityInPacketTypeId),
+                IsTemporary = item.IsTemporary
             };
 
             if (relation?.Quantity == null)
@@ -44,7 +45,8 @@ namespace ShoppingList.Mapper
                 StoreId = itemDto.StoreId,
                 Active = itemDto.Active,
                 Comment = itemDto.Comment,
-                QuantityInPacketTypeId = (uint)itemDto.QuantityInPacketType
+                QuantityInPacketTypeId = (uint)itemDto.QuantityInPacketType,
+                IsTemporary = itemDto.IsTemporary
             };
         }
 
