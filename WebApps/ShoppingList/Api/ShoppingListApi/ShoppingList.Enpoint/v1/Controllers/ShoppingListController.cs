238f4edc0b15af2c5a80b166e34ad3d936690e20
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-20 23:46:50 +0200

    Move controller to Endpoint assembly

--- WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Enpoint/v1/Controllers/ShoppingListController.cs
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Enpoint/v1/Controllers/ShoppingListController.cs
@@ -6,5 +6,10 @@ namespace ShoppingList.WebApp.Controllers
     [Route("v1/shopping-list")]
     public class ShoppingListController : ControllerBase
     {
+        [HttpGet]
+        [Route("active-shopping-list/{storeId}")]
+        public Task<IActionResult> GetActiveShoppingListByStoreId([FromRoute("storeId")] int storeId)
+        {
+        }
     }
 }
