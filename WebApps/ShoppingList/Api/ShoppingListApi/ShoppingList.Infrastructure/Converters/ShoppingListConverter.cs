5368729021c42a15ac4bdf3ac270d722641f1d18
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-20 23:45:43 +0200

    Implement basic model-entity converters

---
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Infrastructure/Converters/ShoppingListConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Models = ShoppingList.Domain.Models;
+
+namespace ShoppingList.Infrastructure.Converters
+{
+    public static class ShoppingListConverter
+    {
+        public static Models.ShoppingList ToDomain(this Entities.ShoppingList entity)
+        {
+            return new Models.ShoppingList(
+                new Models.ShoppingListId(entity.Id),
+                entity.Store.ToDomain(),
+                entity.ItemsOnList.Select(map => map.Item.ToDomain(entity.StoreId, entity.Id)),
+                entity.CompletionDate);
+        }
+
+        public static Entities.ShoppingList ToEntity(this Models.ShoppingList model)
+        {
+            return new Entities.ShoppingList()
+            {
+                Id = model.Id.Value,
+                CompletionDate = model.CompletionDate,
+                StoreId = model.Store.Id.Value
+            };
+        }
+    }
+}
