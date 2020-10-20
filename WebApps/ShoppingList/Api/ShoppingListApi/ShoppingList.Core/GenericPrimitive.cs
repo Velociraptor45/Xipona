57a229c37b71a9d13dd1d31e45fecbd013ef95b9
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-20 22:18:23 +0200

    Convert value field into property in GenericPrimitive

--- WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Core/GenericPrimitive.cs
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Core/GenericPrimitive.cs
@@ -1,23 +1,37 @@
-﻿namespace ShoppingList.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingList.Core
 {
     public class GenericPrimitive<T>
         where T : struct
     {
-        protected readonly T value;
+        public T Value { get; }
 
         public GenericPrimitive(T value)
         {
-            this.value = value;
+            this.Value = value;
         }
 
         public static bool operator ==(GenericPrimitive<T> left, GenericPrimitive<T> right)
         {
-            return left.value.Equals(right.value);
+            return left.Value.Equals(right.Value);
         }
 
         public static bool operator !=(GenericPrimitive<T> left, GenericPrimitive<T> right)
         {
-            return !left.value.Equals(right.value);
+            return !left.Value.Equals(right.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is GenericPrimitive<T> primitive &&
+                   EqualityComparer<T>.Default.Equals(Value, primitive.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Value);
         }
     }
 }
