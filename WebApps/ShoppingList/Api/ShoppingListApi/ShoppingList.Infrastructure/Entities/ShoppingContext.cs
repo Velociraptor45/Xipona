bb8389943b20dbffe1bdefa28729795db8296eb0
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-20 14:31:04 +0200

    Add dbcontext

---
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Infrastructure/Entities/ShoppingContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingList.Infrastructure.Entities
+{
+    public class ShoppingContext : DbContext
+    {
+        public DbSet<AvailableAt> AvailableAts { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemCategory> ItemCategories { get; set; }
+        public DbSet<ItemsOnList> ItemsOnLists { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"server=;port=15909;user id=root;pwd=;database=shoppinglist-development");
+        }
+    }
+}
