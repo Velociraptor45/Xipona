464a0cb77a670efa9e5e36ab0bbd5b27c6221fd2
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-20 20:51:07 +0200

    Add ShoppingListController

---
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Api/v1/Controllers/ShoppingListController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingList.WebApp.Controllers
+{
+    [ApiController]
+    [Route("v1/shopping-list")]
+    public class ShoppingListController : ControllerBase
+    {
+    }
+}
