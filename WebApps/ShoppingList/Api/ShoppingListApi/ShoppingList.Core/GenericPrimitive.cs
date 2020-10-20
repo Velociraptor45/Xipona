65548eba9db4a8ffac486fb5abe0c7a26e86454f
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-20 20:51:46 +0200

    Add GenericPrimitive class

---
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Core/GenericPrimitive.cs
@@ -0,0 +1,23 @@
+﻿namespace ShoppingList.Core
+{
+    public class GenericPrimitive<T>
+        where T : struct
+    {
+        protected readonly T value;
+
+        public GenericPrimitive(T value)
+        {
+            this.value = value;
+        }
+
+        public static bool operator ==(GenericPrimitive<T> left, GenericPrimitive<T> right)
+        {
+            return left.value.Equals(right.value);
+        }
+
+        public static bool operator !=(GenericPrimitive<T> left, GenericPrimitive<T> right)
+        {
+            return !left.value.Equals(right.value);
+        }
+    }
+}
