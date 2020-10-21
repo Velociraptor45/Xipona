34d5f87b09ba4ef0a851475aaba5abf15d4ab8a7
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-21 13:12:47 +0200

    Implement ActiveShoppingListByStoreIdQuery and QueryHandler

---
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Domain/Queries/ActiveShoppingListByStoreId/ActiveShoppingListByStoreIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingList.Domain.Models;
+using ShoppingList.Domain.Queries.SharedModels;
+
+namespace ShoppingList.Domain.Queries.ActiveShoppingListByStoreId
+{
+    public class ActiveShoppingListByStoreIdQuery : IQuery<ShoppingListReadModel>
+    {
+        public ActiveShoppingListByStoreIdQuery(StoreId storeId)
+        {
+            StoreId = storeId;
+        }
+
+        public StoreId StoreId { get; }
+    }
+}
---
+++ WebApps/ShoppingList/Api/ShoppingListApi/ShoppingList.Domain/Queries/ActiveShoppingListByStoreId/ActiveShoppingListByStoreIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using ShoppingList.Domain.Converters;
+using ShoppingList.Domain.Ports;
+using ShoppingList.Domain.Queries.SharedModels;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Domain.Queries.ActiveShoppingListByStoreId
+{
+    public class ActiveShoppingListByStoreIdQueryHandler
+        : IQueryHandler<ActiveShoppingListByStoreIdQuery, ShoppingListReadModel>
+    {
+        private readonly IShoppingListRepository shoppingListRepository;
+
+        public ActiveShoppingListByStoreIdQueryHandler(IShoppingListRepository shoppingListRepository)
+        {
+            this.shoppingListRepository = shoppingListRepository;
+        }
+
+        public async Task<ShoppingListReadModel> HandleAsync(ActiveShoppingListByStoreIdQuery query,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var listModel = await shoppingListRepository.FindActiveByStoreIdAsync(query.StoreId, cancellationToken);
+
+            if (listModel == null)
+                throw new ArgumentException($"No shopping list for store {query.StoreId} found.");
+
+            return listModel.ToReadModel();
+        }
+    }
+}
