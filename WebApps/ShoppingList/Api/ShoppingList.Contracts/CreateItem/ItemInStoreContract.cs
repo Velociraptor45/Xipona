749d9c6f88cbc6b803edcb8375f359472bed7baa
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-23 15:45:24 +0200

    Implement contracts for adding new item

---
+++ WebApps/ShoppingList/Api/ShoppingList.Contracts/CreateItem/ItemInStoreContract.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingList.Contracts.CreateItem
+{
+    public class ItemInStoreContract
+    {
+        public int StoreId { get; set; }
+        public float Price { get; set; }
+    }
+}
