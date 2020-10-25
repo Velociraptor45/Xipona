5769366a1bcc341bea54e5d559c07d1f6aca6a17
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-25 18:57:45 +0100

    Rename WebApp parent folder

--- WebApps/ShoppingList/Frontend/ShoppingList.Frontend.WebApp/Program.cs
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.WebApp/Program.cs
@@ -13,7 +13,7 @@ namespace ShoppingList.Frontend.WebApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("") }); //todo
 
             await builder.Build().RunAsync();
         }
