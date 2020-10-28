3c4411ae62fbedc87709ea27c9a034680171ad40
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-28 11:44:38 +0100

    Implement searching for items

---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/Index/Search/SearchBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingList.Frontend.Models.Index.Search
+{
+    public class SearchBar
+    {
+        public SearchBar()
+        {
+            ResetInput();
+            ResetOptions();
+            Active = false;
+        }
+
+        public string Input { get; set; }
+        public bool Active { get; set; }
+        public IEnumerable<ItemSearchResult> Options { get; set; }
+
+        public void ResetInput()
+        {
+            Input = "";
+        }
+
+        public void ResetOptions()
+        {
+            Options = Enumerable.Empty<ItemSearchResult>();
+        }
+    }
+}
