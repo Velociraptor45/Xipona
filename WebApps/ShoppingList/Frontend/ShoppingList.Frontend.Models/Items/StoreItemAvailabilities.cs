1d641a98708baf7bf86610f31aabbc3396fe3eb7
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-30 10:41:34 +0100

    Implement StoreItem model

---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/Items/StoreItemAvailabilities.cs
@@ -0,0 +1,14 @@
+﻿namespace ShoppingList.Frontend.Models.Items
+{
+    public class StoreItemAvailabilities
+    {
+        public StoreItemAvailabilities(int storeId, float pricePerQuantity)
+        {
+            StoreId = storeId;
+            PricePerQuantity = pricePerQuantity;
+        }
+
+        public int StoreId { get; set; }
+        public float PricePerQuantity { get; set; }
+    }
+}
