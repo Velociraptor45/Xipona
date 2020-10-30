66617191959cabbbce260f3745d791bdc452fa1c
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-10-30 11:53:46 +0100

    Implement Manufacturer and ItemCategory model

---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/ItemCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace ShoppingList.Frontend.Models
+{
+    public class ItemCategory
+    {
+        public ItemCategory(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
