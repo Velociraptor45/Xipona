0b29d3be161dddd0c99bdea3a83d609454732a13
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-11 18:46:41 +0100

    Implement database transaction

---
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain/Ports/Infrastructure/ITransactionGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Api.Domain.Ports.Infrastructure
+{
+    public interface ITransactionGenerator
+    {
+        Task<ITransaction> GenerateAsync(CancellationToken cancellationToken);
+    }
+}
