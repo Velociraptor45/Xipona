88fb8fec959fba9914b12d36d91cc19dd17f21e8
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-14 14:02:13 +0100

    Implement classes for quantity types

---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/QuantityInPacketType.cs
@@ -0,0 +1,14 @@
+﻿namespace ShoppingList.Frontend.Models
+{
+    public class QuantityInPacketType
+    {
+        public QuantityInPacketType(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/QuantityType.cs
@@ -0,0 +1,14 @@
+﻿namespace ShoppingList.Frontend.Models
+{
+    public class QuantityType
+    {
+        public QuantityType(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
