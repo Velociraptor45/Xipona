9d05fdccd49eb23b1570ae5949fe341dd7e6bea8
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-14 13:43:56 +0100

    Add 'IsDeleted' property to StoreItem model

--- WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/Items/StoreItem.cs
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/Items/StoreItem.cs
@@ -4,12 +4,13 @@ namespace ShoppingList.Frontend.Models.Items
 {
     public class StoreItem
     {
-        public StoreItem(int id, string name, string comment, bool isTemporary, int quantityType,
+        public StoreItem(int id, string name, bool isDeleted, string comment, bool isTemporary, int quantityType,
             float quantityInPacket, int quantityInPacketType, int itemCategoryId, int manufacturerId,
-            IEnumerable<StoreItemAvailabilities> availabilities)
+            IEnumerable<StoreItemAvailability> availabilities)
         {
             Id = id;
             Name = name;
+            IsDeleted = isDeleted;
             Comment = comment;
             IsTemporary = isTemporary;
             QuantityType = quantityType;
@@ -22,6 +23,7 @@ namespace ShoppingList.Frontend.Models.Items
 
         public int Id { get; }
         public string Name { get; set; }
+        public bool IsDeleted { get; set; }
         public string Comment { get; set; }
         public bool IsTemporary { get; set; }
         public int QuantityType { get; set; }
@@ -29,6 +31,6 @@ namespace ShoppingList.Frontend.Models.Items
         public int QuantityInPacketType { get; set; }
         public int ItemCategoryId { get; set; }
         public int ManufacturerId { get; set; }
-        public IEnumerable<StoreItemAvailabilities> Availabilities { get; set; }
+        public IEnumerable<StoreItemAvailability> Availabilities { get; set; }
     }
 }
