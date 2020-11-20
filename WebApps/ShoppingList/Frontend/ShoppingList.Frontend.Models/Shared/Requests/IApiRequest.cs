3c23441c4b4a9efda4b47dc2461f0196a6036976
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-20 22:47:37 +0100

    Add first api request class

---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/Shared/Requests/IApiRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ShoppingList.Frontend.Models.Shared.Requests
+{
+    public interface IApiRequest
+    {
+        public Guid RequestId { get; }
+    }
+}
---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Models/Shared/Requests/PutItemInBasketRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShoppingList.Frontend.Models.Shared.Requests
+{
+    public class PutItemInBasketRequest : IApiRequest
+    {
+        public PutItemInBasketRequest(Guid requestId, int shoppingListId, int itemId)
+        {
+            RequestId = requestId;
+            ShoppingListId = shoppingListId;
+            ItemId = itemId;
+        }
+
+        public Guid RequestId { get; }
+        public int ShoppingListId { get; }
+        public int ItemId { get; }
+    }
+}
