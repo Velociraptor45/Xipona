de41502d032408c1ac838b7a21321435db28a8c1
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-20 22:45:20 +0100

    Add CommandClient and interface class files

---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Infrastructure/Connection/CommandClient.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingList.Frontend.Models.Shared.Requests;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Frontend.Infrastructure.Connection
+{
+    public class CommandClient : ICommandClient
+    {
+        public Task IsAliveAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task PutItemInBasketAsync(PutItemInBasketRequest request)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebApps/ShoppingList/Frontend/ShoppingList.Frontend.Infrastructure/Connection/ICommandClient.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingList.Frontend.Models.Shared.Requests;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Frontend.Infrastructure.Connection
+{
+    public interface ICommandClient
+    {
+        Task IsAliveAsync();
+
+        Task PutItemInBasketAsync(PutItemInBasketRequest request);
+    }
+}
