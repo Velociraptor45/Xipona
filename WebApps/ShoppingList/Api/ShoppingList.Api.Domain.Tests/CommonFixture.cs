c0efa68d298c2a4efca7b769e2ce7c24760a7283
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-27 19:54:45 +0100

    Implement CommonFixture class

---
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain.Tests/CommonFixture.cs
@@ -0,0 +1,23 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using System;
+
+namespace ProjectHermes.ShoppingList.Api.Domain.Tests
+{
+    public static class CommonFixture
+    {
+        private static readonly Random random = new Random();
+
+        public static Fixture GetNewFixture()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoMoqCustomization { ConfigureMembers = true });
+            return fixture;
+        }
+
+        public static int NextInt()
+        {
+            return random.Next(1, int.MaxValue);
+        }
+    }
+}
