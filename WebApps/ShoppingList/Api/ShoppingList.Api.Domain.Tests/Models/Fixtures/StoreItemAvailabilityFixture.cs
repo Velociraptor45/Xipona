1c0931a8f02fb0cba9fe7d7f65e0f82d6093dafb
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-28 11:17:04 +0100

    Create StoreItemAvailabilityFixture class

---
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain.Tests/Models/Fixtures/StoreItemAvailabilityFixture.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using ShoppingList.Api.Domain.Models;
+
+namespace ProjectHermes.ShoppingList.Api.Domain.Tests.Models.Fixtures
+{
+    public class StoreItemAvailabilityFixture
+    {
+        private readonly CommonFixture commonFixture;
+
+        public StoreItemAvailabilityFixture()
+        {
+            commonFixture = new CommonFixture();
+        }
+
+        public StoreItemAvailability GetAvailability(StoreId storeId)
+        {
+            var fixture = commonFixture.GetNewFixture();
+            fixture.Inject(storeId);
+            return fixture.Create<StoreItemAvailability>();
+        }
+
+        public StoreItemAvailability GetAvailability(int storeId)
+        {
+            return GetAvailability(new StoreId(storeId));
+        }
+
+        public StoreItemAvailability GetAvailability()
+        {
+            return GetAvailability(commonFixture.NextInt());
+        }
+    }
+}
