83ed95035202331cbf21a7a69e0c966161e0f9bd
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-29 11:25:10 +0100

    Implement loading actual id when given offline id in PutItemInBasketCommandHandler

--- WebApps/ShoppingList/Api/ShoppingList.Api.Domain.Tests/Commands/PutItemInBasket/PutItemInBasketCommandHandlerTests.cs
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain.Tests/Commands/PutItemInBasket/PutItemInBasketCommandHandlerTests.cs
@@ -23,12 +23,16 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.Commands.PutItemInBasket
         private readonly CommonFixture commonFixture;
         private readonly ShoppingListItemFixture shoppingListItemFixture;
         private readonly ShoppingListFixture shoppingListFixture;
+        private readonly StoreItemAvailabilityFixture storeItemAvailabilityFixture;
+        private readonly StoreItemFixture storeItemFixture;
 
         public PutItemInBasketCommandHandlerTests()
         {
             commonFixture = new CommonFixture();
             shoppingListItemFixture = new ShoppingListItemFixture(commonFixture);
             shoppingListFixture = new ShoppingListFixture(shoppingListItemFixture, commonFixture);
+            storeItemAvailabilityFixture = new StoreItemAvailabilityFixture(commonFixture);
+            storeItemFixture = new StoreItemFixture(storeItemAvailabilityFixture, commonFixture);
         }
 
         [Fact]
@@ -79,6 +83,59 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.Commands.PutItemInBasket
                 shoppingListRepositoryMock.Verify(
                     i => i.StoreAsync(
                         It.Is<DomainModels.ShoppingList>(list => list.Id == shoppingList.Id),
+                        It.IsAny<CancellationToken>()),
+                    Times.Once);
+            }
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithValidOfflineIdCommand_ShouldPutItemInBasket()
+        {
+            // Arrange
+            var fixture = commonFixture.GetNewFixture();
+            var shoppingListRepositoryMock = fixture.Freeze<Mock<IShoppingListRepository>>();
+            var itemRepositoryMock = fixture.Freeze<Mock<IItemRepository>>();
+
+            var itemNotInBasket = shoppingListItemFixture.GetShoppingListItem(isInBasket: false);
+            var shoppingList = shoppingListFixture.GetShoppingList(itemCount: 0, itemNotInBasket.ToMonoList());
+            var listItemIdOffline = new ShoppingListItemId(Guid.NewGuid());
+            var storeItemIdActual = new StoreItemId(itemNotInBasket.Id.Actual.Value);
+            var storeItemIdOffline = new StoreItemId(listItemIdOffline.Offline.Value);
+            var storeItem = storeItemFixture.GetStoreItem(storeItemIdActual);
+
+            fixture.ConstructorArgumentFor<PutItemInBasketCommand, ShoppingListItemId>("itemId", listItemIdOffline);
+            var command = fixture.Create<PutItemInBasketCommand>();
+            var handler = fixture.Create<PutItemInBasketCommandHandler>();
+
+            shoppingListRepositoryMock
+                .Setup(instance => instance.FindByAsync(
+                    It.Is<ShoppingListId>(id => id == command.ShoppingListId),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(shoppingList));
+
+            itemRepositoryMock
+                .Setup(instance => instance.FindByAsync(
+                    It.Is<StoreItemId>(id => id.Offline.Value == listItemIdOffline.Offline.Value),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(storeItem));
+
+            // Act
+            bool result = await handler.HandleAsync(command, default);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.Should().BeTrue();
+                shoppingList.Items.Should().ContainSingle();
+                shoppingList.Items.First().IsInBasket.Should().BeTrue();
+                shoppingListRepositoryMock.Verify(
+                    i => i.StoreAsync(
+                        It.Is<DomainModels.ShoppingList>(list => list.Id == shoppingList.Id),
+                        It.IsAny<CancellationToken>()),
+                    Times.Once);
+                itemRepositoryMock.Verify(
+                    i => i.FindByAsync(
+                        It.Is<StoreItemId>(id => id == storeItemIdOffline),
                         It.IsAny<CancellationToken>()),
                     Times.Once);
             }
--- WebApps/ShoppingList/Api/ShoppingList.Api.Domain/Commands/PutItemInBasket/PutItemInBasketCommandHandler.cs
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain/Commands/PutItemInBasket/PutItemInBasketCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using ShoppingList.Api.Domain.Ports;
+﻿using ShoppingList.Api.Domain.Extensions;
+using ShoppingList.Api.Domain.Models;
+using ShoppingList.Api.Domain.Ports;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,10 +10,13 @@ namespace ShoppingList.Api.Domain.Commands.PutItemInBasket
     public class PutItemInBasketCommandHandler : ICommandHandler<PutItemInBasketCommand, bool>
     {
         private readonly IShoppingListRepository shoppingListRepository;
+        private readonly IItemRepository itemRepository;
 
-        public PutItemInBasketCommandHandler(IShoppingListRepository shoppingListRepository)
+        public PutItemInBasketCommandHandler(IShoppingListRepository shoppingListRepository,
+            IItemRepository itemRepository)
         {
             this.shoppingListRepository = shoppingListRepository;
+            this.itemRepository = itemRepository;
         }
 
         public async Task<bool> HandleAsync(PutItemInBasketCommand command, CancellationToken cancellationToken)
@@ -20,7 +25,15 @@ namespace ShoppingList.Api.Domain.Commands.PutItemInBasket
                 throw new ArgumentNullException(nameof(command));
 
             var shoppingList = await shoppingListRepository.FindByAsync(command.ShoppingListId, cancellationToken);
-            shoppingList.PutItemInBasket(command.ItemId);
+            ShoppingListItemId itemId = command.ItemId;
+
+            if (!itemId.IsActualId)
+            {
+                itemId = (await itemRepository.FindByAsync(command.ItemId.ToStoreItemId(), cancellationToken))
+                    .Id.ToShoppingListItemId();
+            }
+
+            shoppingList.PutItemInBasket(itemId);
 
             cancellationToken.ThrowIfCancellationRequested();
 
