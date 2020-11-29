858e564c8c5387d75456b8c55938802f50b2079d
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-11-29 11:00:18 +0100

    Implemen unit tests for PutItemInBasketCommandHandler

---
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain.Tests/Commands/PutItemInBasket/PutItemInBasketCommandHandlerTests.cs
@@ -0,0 +1,87 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Moq;
+using ProjectHermes.ShoppingList.Api.Domain.Tests.Models.Fixtures;
+using ShoppingList.Api.Core.Extensions;
+using ShoppingList.Api.Core.Tests.AutoFixture;
+using ShoppingList.Api.Domain.Commands.PutItemInBasket;
+using ShoppingList.Api.Domain.Models;
+using ShoppingList.Api.Domain.Ports;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+using DomainModels = ShoppingList.Api.Domain.Models;
+
+namespace ProjectHermes.ShoppingList.Api.Domain.Tests.Commands.PutItemInBasket
+{
+    public class PutItemInBasketCommandHandlerTests
+    {
+        private readonly CommonFixture commonFixture;
+        private readonly ShoppingListItemFixture shoppingListItemFixture;
+        private readonly ShoppingListFixture shoppingListFixture;
+
+        public PutItemInBasketCommandHandlerTests()
+        {
+            commonFixture = new CommonFixture();
+            shoppingListItemFixture = new ShoppingListItemFixture(commonFixture);
+            shoppingListFixture = new ShoppingListFixture(shoppingListItemFixture, commonFixture);
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithCommandIsNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var fixture = commonFixture.GetNewFixture();
+            var handler = fixture.Create<PutItemInBasketCommandHandler>();
+
+            // Act
+            Func<Task> function = async () => await handler.HandleAsync(null, default);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                await function.Should().ThrowAsync<ArgumentNullException>();
+            }
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithValidActualIdCommand_ShouldPutItemInBasket()
+        {
+            // Arrange
+            var fixture = commonFixture.GetNewFixture();
+            var itemNotInBasket = shoppingListItemFixture.GetShoppingListItem(isInBasket: false);
+            var shoppingList = shoppingListFixture.GetShoppingList(itemCount: 0, itemNotInBasket.ToMonoList());
+            var shoppingListRepositoryMock = fixture.Freeze<Mock<IShoppingListRepository>>();
+
+            fixture.ConstructorArgumentFor<PutItemInBasketCommand, ShoppingListItemId>("itemId", itemNotInBasket.Id);
+            var command = fixture.Create<PutItemInBasketCommand>();
+            var handler = fixture.Create<PutItemInBasketCommandHandler>();
+
+            shoppingListRepositoryMock
+                .Setup(instance => instance.FindByAsync(
+                    It.Is<ShoppingListId>(id => id == command.ShoppingListId),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(shoppingList));
+
+            // Act
+            bool result = await handler.HandleAsync(command, default);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.Should().BeTrue();
+                shoppingList.Items.Should().ContainSingle();
+                shoppingList.Items.First().IsInBasket.Should().BeTrue();
+                shoppingListRepositoryMock.Verify(
+                    i => i.StoreAsync(
+                        It.Is<DomainModels.ShoppingList>(list => list.Id == shoppingList.Id),
+                        It.IsAny<CancellationToken>()),
+                    Times.Once);
+            }
+        }
+    }
+}
--- WebApps/ShoppingList/Api/ShoppingList.Api.Domain/Commands/PutItemInBasket/PutItemInBasketCommandHandler.cs
+++ WebApps/ShoppingList/Api/ShoppingList.Api.Domain/Commands/PutItemInBasket/PutItemInBasketCommandHandler.cs
@@ -7,24 +7,24 @@ namespace ShoppingList.Api.Domain.Commands.PutItemInBasket
 {
     public class PutItemInBasketCommandHandler : ICommandHandler<PutItemInBasketCommand, bool>
     {
+        private readonly IShoppingListRepository shoppingListRepository;
+
         public PutItemInBasketCommandHandler(IShoppingListRepository shoppingListRepository)
         {
-            ShoppingListRepository = shoppingListRepository;
+            this.shoppingListRepository = shoppingListRepository;
         }
-
-        public IShoppingListRepository ShoppingListRepository { get; }
 
         public async Task<bool> HandleAsync(PutItemInBasketCommand command, CancellationToken cancellationToken)
         {
             if (command == null)
                 throw new ArgumentNullException(nameof(command));
 
-            var shoppingList = await ShoppingListRepository.FindByAsync(command.ShoppingListId, cancellationToken);
-            //shoppingList.PutItemInBasket(command.ItemId);
+            var shoppingList = await shoppingListRepository.FindByAsync(command.ShoppingListId, cancellationToken);
+            shoppingList.PutItemInBasket(command.ItemId);
 
             cancellationToken.ThrowIfCancellationRequested();
 
-            await ShoppingListRepository.StoreAsync(shoppingList, cancellationToken);
+            await shoppingListRepository.StoreAsync(shoppingList, cancellationToken);
 
             return true;
         }
