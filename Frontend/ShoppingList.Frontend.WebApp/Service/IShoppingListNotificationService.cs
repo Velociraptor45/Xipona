3575a70c636292003e4ac8b967b873d7bab3f2c6
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-12-13 19:24:24 +0100

    Implement notification service

---
+++ Frontend/ShoppingList.Frontend.WebApp/Service/IShoppingListNotificationService.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectHermes.ShoppingList.Frontend.WebApp.Service
+{
+    public interface IShoppingListNotificationService
+    {
+        void Notify(string title, string message);
+
+        void NotifySuccess(string title, string message);
+
+        void NotifyWarning(string title, string message);
+
+        void NotifyError(string title, string message);
+    }
+}
---
+++ Frontend/ShoppingList.Frontend.WebApp/Service/ShoppingListNotificationService.cs
@@ -0,0 +1,53 @@
+﻿using AntDesign;
+
+namespace ProjectHermes.ShoppingList.Frontend.WebApp.Service
+{
+    public class ShoppingListNotificationService : IShoppingListNotificationService
+    {
+        private readonly NotificationService notificationService;
+
+        public ShoppingListNotificationService(NotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
+        public void Notify(string title, string message)
+        {
+            notificationService.Open(new NotificationConfig
+            {
+                Message = title,
+                Description = message
+            });
+        }
+
+        public void NotifySuccess(string title, string message)
+        {
+            notificationService.Open(new NotificationConfig
+            {
+                Message = title,
+                Description = message,
+                NotificationType = NotificationType.Success
+            });
+        }
+
+        public void NotifyWarning(string title, string message)
+        {
+            notificationService.Open(new NotificationConfig
+            {
+                Message = title,
+                Description = message,
+                NotificationType = NotificationType.Warning
+            });
+        }
+
+        public void NotifyError(string title, string message)
+        {
+            notificationService.Open(new NotificationConfig
+            {
+                Message = title,
+                Description = message,
+                NotificationType = NotificationType.Error
+            });
+        }
+    }
+}
