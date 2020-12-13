122d44a4defad789ec77c6753130c82f4b0d3218
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-12-13 16:31:45 +0100

    Implement unit test for Delete method in StoreItem model

---
+++ Api/ShoppingList.Api.Domain.Tests/StoreItems/Models/StoreItems/StoreItemTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Tests.Common.Fixtures;
+using Xunit;
+
+namespace ProjectHermes.ShoppingList.Api.Domain.Tests.StoreItems.Models.StoreItems
+{
+    public class StoreItemTests
+    {
+        private readonly CommonFixture commonFixture;
+        private readonly StoreItemAvailabilityFixture storeItemAvailabilityFixture;
+        private readonly StoreItemFixture storeItemFixture;
+
+        public StoreItemTests()
+        {
+            commonFixture = new CommonFixture();
+            storeItemAvailabilityFixture = new StoreItemAvailabilityFixture(commonFixture);
+            storeItemFixture = new StoreItemFixture(storeItemAvailabilityFixture, commonFixture);
+        }
+
+        #region Delete
+
+        [Fact]
+        public void Delete_WithNotDeltedStoreItem_ShouldMarkStoreItemAsDeleted()
+        {
+            // Arrange
+            var storeItem = storeItemFixture.GetStoreItem(new StoreItemId(commonFixture.NextInt()), isDeleted: false);
+
+            // Act
+            storeItem.Delete();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                storeItem.IsDeleted.Should().BeTrue();
+            }
+        }
+
+        #endregion Delete
+    }
+}
