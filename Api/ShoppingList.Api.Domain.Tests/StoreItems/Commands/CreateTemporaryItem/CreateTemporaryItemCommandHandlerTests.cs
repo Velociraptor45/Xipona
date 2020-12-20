25ce96e8b4689824c453db1b0e6fb2c4cd32ae11
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2020-12-20 14:31:39 +0100

    Make more tests go green

--- Api/ShoppingList.Api.Domain.Tests/StoreItems/Commands/CreateTemporaryItem/CreateTemporaryItemCommandHandlerTests.cs
+++ Api/ShoppingList.Api.Domain.Tests/StoreItems/Commands/CreateTemporaryItem/CreateTemporaryItemCommandHandlerTests.cs
@@ -2,11 +2,14 @@
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Moq;
+using ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions;
+using ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions.Reason;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Models;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Ports;
 using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Commands.CreateTemporaryItem;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models.Factories;
 using ProjectHermes.ShoppingList.Api.Domain.Tests.Common.Fixtures;
-using ShoppingList.Api.Domain.Models;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,11 +21,13 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.StoreItems.Commands.Create
     {
         private readonly CommonFixture commonFixture;
         private readonly StoreFixture storeFixture;
+        private readonly StoreItemFixture storeItemFixture;
 
         public CreateTemporaryItemCommandHandlerTests()
         {
             commonFixture = new CommonFixture();
             storeFixture = new StoreFixture(commonFixture);
+            storeItemFixture = new StoreItemFixture(new StoreItemAvailabilityFixture(commonFixture), commonFixture);
         }
 
         [Fact]
@@ -44,14 +49,43 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.StoreItems.Commands.Create
         }
 
         [Fact]
-        public async Task HandleAsync_WithValidCommand_ShouldStoreItemAndReturnTrue()
+        public async Task HandleAsync_WithDeletedStore_ShouldThrowDomainException()
         {
             // Arrange
             var fixture = commonFixture.GetNewFixture();
-            IStore store = storeFixture.GetStore(isDeleted: false);
 
-            var itemRepositoryMock = fixture.Freeze<Mock<IItemRepository>>();
-            var storeRepositoryMock = fixture.Freeze<Mock<IStoreRepository>>();
+            Mock<IStoreRepository> storeRepositoryMock = fixture.Freeze<Mock<IStoreRepository>>();
+
+            var handler = fixture.Create<CreateTemporaryItemCommandHandler>();
+            var command = fixture.Create<CreateTemporaryItemCommand>();
+
+            IStore store = storeFixture.GetStore(command.TemporaryItemCreation.Availability.StoreId, isDeleted: true);
+
+            storeRepositoryMock
+                .Setup(i => i.FindByAsync(
+                    It.Is<StoreId>(id => id == command.TemporaryItemCreation.Availability.StoreId),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(store));
+
+            // Act
+            Func<Task<bool>> action = async () => await handler.HandleAsync(command, default);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                (await action.Should().ThrowAsync<DomainException>())
+                    .Where(e => e.Reason.ErrorCode == ErrorReasonCode.StoreNotFound);
+            }
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithStoreIsNull_ShouldThrowDomainException()
+        {
+            // Arrange
+            var fixture = commonFixture.GetNewFixture();
+
+            Mock<IStoreRepository> storeRepositoryMock = fixture.Freeze<Mock<IStoreRepository>>();
+
             var handler = fixture.Create<CreateTemporaryItemCommandHandler>();
             var command = fixture.Create<CreateTemporaryItemCommand>();
 
@@ -59,7 +93,45 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.StoreItems.Commands.Create
                 .Setup(i => i.FindByAsync(
                     It.Is<StoreId>(id => id == command.TemporaryItemCreation.Availability.StoreId),
                     It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IStore>(null));
+
+            // Act
+            Func<Task<bool>> action = async () => await handler.HandleAsync(command, default);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                (await action.Should().ThrowAsync<DomainException>())
+                    .Where(e => e.Reason.ErrorCode == ErrorReasonCode.StoreNotFound);
+            }
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithValidCommand_ShouldStoreItem()
+        {
+            // Arrange
+            var fixture = commonFixture.GetNewFixture();
+
+            Mock<IStoreItemFactory> storeItemFactoryMock = fixture.Freeze<Mock<IStoreItemFactory>>();
+            Mock<IItemRepository> itemRepositoryMock = fixture.Freeze<Mock<IItemRepository>>();
+            Mock<IStoreRepository> storeRepositoryMock = fixture.Freeze<Mock<IStoreRepository>>();
+
+            var handler = fixture.Create<CreateTemporaryItemCommandHandler>();
+            var command = fixture.Create<CreateTemporaryItemCommand>();
+
+            IStoreItem storeItem = storeItemFixture.GetStoreItem();
+            IStore store = storeFixture.GetStore(command.TemporaryItemCreation.Availability.StoreId, isDeleted: false);
+
+            storeRepositoryMock
+                .Setup(i => i.FindByAsync(
+                    It.Is<StoreId>(id => id == command.TemporaryItemCreation.Availability.StoreId),
+                    It.IsAny<CancellationToken>()))
                 .Returns(Task.FromResult(store));
+
+            storeItemFactoryMock
+                .Setup(i => i.Create(
+                    It.Is<TemporaryItemCreation>(obj => obj == command.TemporaryItemCreation)))
+                .Returns(storeItem);
 
             // Act
             var result = await handler.HandleAsync(command, default);
@@ -69,7 +141,8 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.StoreItems.Commands.Create
             {
                 result.Should().BeTrue();
                 itemRepositoryMock.Verify(
-                    i => i.StoreAsync(It.IsAny<StoreItem>(), It.IsAny<CancellationToken>()),
+                    i => i.StoreAsync(It.Is<IStoreItem>(item => item == storeItem),
+                    It.IsAny<CancellationToken>()),
                     Times.Once);
             }
         }
