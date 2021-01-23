a01fbe13f23eba7248b1a41df0132ea0d57999a7
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-01-23 20:26:59 +0100

    #52: Implement removing items from shopping list when deleting item category

--- Api/ShoppingList.Api.Domain/ItemCategories/Commands/DeleteItemCategory/DeleteItemCategoryCommandHandler.cs
+++ Api/ShoppingList.Api.Domain/ItemCategories/Commands/DeleteItemCategory/DeleteItemCategoryCommandHandler.cs
@@ -3,6 +3,8 @@ using ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions.Reason;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Ports;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Ports.Infrastructure;
+using ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Ports;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models.Extensions;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,13 +15,16 @@ namespace ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Commands.DeleteIt
     {
         private readonly IItemCategoryRepository itemCategoryRepository;
         private readonly IItemRepository itemRepository;
+        private readonly IShoppingListRepository shoppingListRepository;
         private readonly ITransactionGenerator transactionGenerator;
 
         public DeleteItemCategoryCommandHandler(IItemCategoryRepository itemCategoryRepository,
-            IItemRepository itemRepository, ITransactionGenerator transactionGenerator)
+            IItemRepository itemRepository, IShoppingListRepository shoppingListRepository,
+            ITransactionGenerator transactionGenerator)
         {
             this.itemCategoryRepository = itemCategoryRepository;
             this.itemRepository = itemRepository;
+            this.shoppingListRepository = shoppingListRepository;
             this.transactionGenerator = transactionGenerator;
         }
 
@@ -40,12 +45,18 @@ namespace ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Commands.DeleteIt
                 .ToList();
 
             using var transaction = await transactionGenerator.GenerateAsync(cancellationToken);
-            await itemCategoryRepository.StoreAsync(category, cancellationToken);
             foreach (var item in storeItems)
             {
+                var lists = await shoppingListRepository.FindActiveByAsync(item.Id, cancellationToken);
+                foreach (var list in lists)
+                {
+                    list.RemoveItem(item.Id.ToShoppingListItemId());
+                    await shoppingListRepository.StoreAsync(list, cancellationToken);
+                }
                 item.Delete();
                 await itemRepository.StoreAsync(item, cancellationToken);
             }
+            await itemCategoryRepository.StoreAsync(category, cancellationToken);
 
             await transaction.CommitAsync(cancellationToken);
             return true;
