4b49d8cf7560c3ee13c4c6325a64e291fa1dd451
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-01-23 20:28:32 +0100

    #52: Implement setup extension method for FindActiveByAsync of ItemRepository mock

--- Api/ShoppingList.Api.Domain.Tests/Common/Extensions/ItemRepositoryMockExtensions.cs
+++ Api/ShoppingList.Api.Domain.Tests/Common/Extensions/ItemRepositoryMockExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
+using ProjectHermes.ShoppingList.Api.Domain.Common.Models;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Ports;
 using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,6 +18,16 @@ namespace ProjectHermes.ShoppingList.Api.Domain.Tests.Common.Extensions
                     It.Is<StoreItemId>(id => id == storeItemId),
                     It.IsAny<CancellationToken>()))
                 .Returns(Task.FromResult(returnValue));
+        }
+
+        public static void SetupFindActiveByAsync(this Mock<IItemRepository> mock, ItemCategoryId itemCategoryId,
+            IEnumerable<IStoreItem> returnValue)
+        {
+            mock
+                .Setup(i => i.FindActiveByAsync(
+                    It.Is<ItemCategoryId>(id => id == itemCategoryId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(returnValue);
         }
     }
 }
