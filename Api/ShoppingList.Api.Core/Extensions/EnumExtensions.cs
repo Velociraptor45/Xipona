bd199fcfbdfa36ea6ce4f4abfc637a934794f42e
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-01 20:36:57 +0100

    #61: Implement enum / int extensions

--- Api/ShoppingList.Api.Core/Extensions/EnumExtensions.cs
+++ Api/ShoppingList.Api.Core/Extensions/EnumExtensions.cs
@@ -13,5 +13,16 @@ namespace ProjectHermes.ShoppingList.Api.Core.Extensions
             return type.GetField(name)
                 .GetCustomAttribute<TAttribute>();
         }
+
+        public static int ToInt<T>(this T value)
+            where T : Enum
+        {
+            int intValue = (int)(object)value;
+
+            if (!Enum.IsDefined(typeof(T), intValue))
+                throw new InvalidOperationException($"{intValue} is an invalid {typeof(T).FullName} value.");
+
+            return intValue;
+        }
     }
 }
