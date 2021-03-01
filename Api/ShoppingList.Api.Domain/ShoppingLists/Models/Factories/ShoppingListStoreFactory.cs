6bb183da8d97abd29b140085263857a1faf39b21
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-01 20:36:29 +0100

    #61: Implement ShoppingListStoreFactory

---
+++ Api/ShoppingList.Api.Domain/ShoppingLists/Models/Factories/ShoppingListStoreFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Models.Factories
+{
+    public class ShoppingListStoreFactory : IShoppingListStoreFactory
+    {
+        public IShoppingListStore Create(ShoppingListStoreId id, string name, bool isDeleted)
+        {
+            return new ShoppingListStore(
+                id,
+                name,
+                isDeleted);
+        }
+    }
+}
