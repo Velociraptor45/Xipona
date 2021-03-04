415481d4a833f5f6711d4dee49aa348de7ce7c76
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-04 20:04:15 +0100

    #61: Load shopping entities not tracked

--- Api/ShoppingList.Api.Infrastructure/Adapters/ShoppingListRepository.cs
+++ Api/ShoppingList.Api.Infrastructure/Adapters/ShoppingListRepository.cs
@@ -181,7 +181,7 @@ namespace ProjectHermes.ShoppingList.Api.Infrastructure.Adapters
 
         private IQueryable<Entities.ShoppingList> GetShoppingListQuery()
         {
-            return dbContext.ShoppingLists
+            return dbContext.ShoppingLists.AsNoTracking()
                 .Include(l => l.Store)
                 .ThenInclude(s => s.Sections)
 
