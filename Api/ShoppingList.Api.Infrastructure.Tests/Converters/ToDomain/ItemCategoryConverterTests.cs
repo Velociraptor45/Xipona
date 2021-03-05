8f5d64c7aa99c47622ccf83efa58debe35cfee6b
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-05 19:32:20 +0100

    #61: Implement to domain converter test for item category and manufacturer

---
+++ Api/ShoppingList.Api.Infrastructure.Tests/Converters/ToDomain/ItemCategoryConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+using ProjectHermes.ShoppingList.Api.Core.Extensions;
+using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models;
+using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models.Factories;
+using ProjectHermes.ShoppingList.Api.Infrastructure.Converters.ToDomain;
+using ShoppingList.Api.Core.TestKit.Converter;
+using ShoppingList.Api.Domain.TestKit.ItemCategories.Fixtures;
+using ShoppingList.Api.Domain.TestKit.Shared;
+using Entities = ProjectHermes.ShoppingList.Api.Infrastructure.Entities;
+
+namespace ShoppingList.Api.Infrastructure.Tests.Converters.ToDomain
+{
+    public class ItemCategoryConverterTests : ToDomainConverterTestBase<Entities.ItemCategory, IItemCategory>
+    {
+        protected override (Entities.ItemCategory, IItemCategory) CreateTestObjects()
+        {
+            var commonFixture = new CommonFixture();
+            var domainFixture = new ItemCategoryFixture(commonFixture);
+
+            var destination = domainFixture.GetItemCategory();
+            var source = new Entities.ItemCategory()
+            {
+                Id = destination.Id.Value,
+                Deleted = destination.IsDeleted,
+                Name = destination.Name
+            };
+
+            return (source, destination);
+        }
+
+        protected override void SetupServiceCollection()
+        {
+            serviceCollection.AddInstancesOfGenericType(typeof(ItemCategoryConverter).Assembly, typeof(IToDomainConverter<,>));
+            serviceCollection.AddInstancesOfNonGenericType(typeof(IItemCategoryFactory).Assembly, typeof(IItemCategoryFactory));
+        }
+    }
+}
---
+++ Api/ShoppingList.Api.Infrastructure.Tests/Converters/ToDomain/ManufacturerConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+using ProjectHermes.ShoppingList.Api.Core.Extensions;
+using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models.Factories;
+using ProjectHermes.ShoppingList.Api.Infrastructure.Converters.ToDomain;
+using ShoppingList.Api.Core.TestKit.Converter;
+using ShoppingList.Api.Domain.TestKit.Manufacturers.Fixtures;
+using ShoppingList.Api.Domain.TestKit.Shared;
+using Entities = ProjectHermes.ShoppingList.Api.Infrastructure.Entities;
+
+namespace ShoppingList.Api.Infrastructure.Tests.Converters.ToDomain
+{
+    public class ManufacturerConverterTests : ToDomainConverterTestBase<Entities.Manufacturer, IManufacturer>
+    {
+        protected override (Entities.Manufacturer, IManufacturer) CreateTestObjects()
+        {
+            var commonFixture = new CommonFixture();
+            var domainFixture = new ManufacturerFixture(commonFixture);
+
+            var destination = domainFixture.Create();
+            var source = new Entities.Manufacturer()
+            {
+                Id = destination.Id.Value,
+                Deleted = destination.IsDeleted,
+                Name = destination.Name
+            };
+
+            return (source, destination);
+        }
+
+        protected override void SetupServiceCollection()
+        {
+            serviceCollection.AddInstancesOfGenericType(typeof(ManufacturerConverter).Assembly, typeof(IToDomainConverter<,>));
+            serviceCollection.AddInstancesOfNonGenericType(typeof(IManufacturerFactory).Assembly, typeof(IManufacturerFactory));
+        }
+    }
+}
