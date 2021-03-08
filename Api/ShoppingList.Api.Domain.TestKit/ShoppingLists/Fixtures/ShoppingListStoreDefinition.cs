d175a8290a7f5a895165e68d5470beae42f34851
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-08 21:37:13 +0100

    #61: Implement ShoppingListStoreFixture

---
+++ Api/ShoppingList.Api.Domain.TestKit/ShoppingLists/Fixtures/ShoppingListStoreDefinition.cs
@@ -0,0 +1,34 @@
+﻿using ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Models;
+
+namespace ShoppingList.Api.Domain.TestKit.ShoppingLists.Fixtures
+{
+    public class ShoppingListStoreDefinition
+    {
+        public ShoppingListStoreId Id { get; set; }
+        public string Name { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public ShoppingListStoreDefinition Clone()
+        {
+            return new ShoppingListStoreDefinition
+            {
+                Id = Id,
+                Name = Name,
+                IsDeleted = IsDeleted
+            };
+        }
+
+        public static ShoppingListStoreDefinition FromId(int id)
+        {
+            return FromId(new ShoppingListStoreId(id));
+        }
+
+        public static ShoppingListStoreDefinition FromId(ShoppingListStoreId id)
+        {
+            return new ShoppingListStoreDefinition
+            {
+                Id = id
+            };
+        }
+    }
+}
