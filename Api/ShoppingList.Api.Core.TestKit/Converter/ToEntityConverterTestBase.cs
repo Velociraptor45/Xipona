d61306b2974298f08eddb96966882db3f49ba6c0
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-09 21:33:39 +0100

    #61: Implement ToEntity converter tests

---
+++ Api/ShoppingList.Api.Core.TestKit/Converter/ToEntityConverterTestBase.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectHermes.ShoppingList.Api.Core.Converter;
+using System;
+using Xunit;
+
+namespace ShoppingList.Api.Core.TestKit.Converter
+{
+    public abstract class ToEntityConverterTestBase<TSource, TDestination>
+        where TSource : class
+        where TDestination : class
+    {
+        protected readonly TSource source;
+        protected readonly TDestination destination;
+        protected readonly IServiceCollection serviceCollection;
+
+        protected ToEntityConverterTestBase()
+        {
+            (source, destination) = CreateTestObjects();
+            serviceCollection = new ServiceCollection();
+            SetupServiceCollection();
+        }
+
+        protected abstract (TSource, TDestination) CreateTestObjects();
+
+        protected abstract void SetupServiceCollection();
+
+        protected IToEntityConverter<TSource, TDestination> CreateConverter()
+        {
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider.GetRequiredService<IToEntityConverter<TSource, TDestination>>();
+        }
+
+        [Fact]
+        public void ToDomain_WithSourceIsNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var converter = CreateConverter();
+
+            // Act
+            Action act = () => converter.ToEntity((TSource)null);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                act.Should().Throw<ArgumentNullException>();
+            }
+        }
+
+        [Fact]
+        public void ToDomain_WithValidSourceObject_ShouldConvertToDestinationObject()
+        {
+            // Arrange
+            var converter = CreateConverter();
+
+            // Act
+            TDestination result = converter.ToEntity(source);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.Should().BeEquivalentTo(destination);
+            }
+        }
+    }
+}
---
+++ Api/ShoppingList.Api.Infrastructure.Tests/Converters/ToEntity/ItemCategoryConverterTests.cs
@@ -0,0 +1,31 @@
+﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+using ProjectHermes.ShoppingList.Api.Core.Extensions;
+using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models;
+using ProjectHermes.ShoppingList.Api.Infrastructure.Converters.ToEntity;
+using ShoppingList.Api.Core.TestKit.Converter;
+using ShoppingList.Api.Domain.TestKit.ItemCategories.Fixtures;
+using ShoppingList.Api.Domain.TestKit.Shared;
+
+using Entities = ProjectHermes.ShoppingList.Api.Infrastructure.Entities;
+
+namespace ShoppingList.Api.Infrastructure.Tests.Converters.ToEntity
+{
+    public class ItemCategoryConverterTests : ToEntityConverterTestBase<IItemCategory, Entities.ItemCategory>
+    {
+        protected override (IItemCategory, Entities.ItemCategory) CreateTestObjects()
+        {
+            var commonFixture = new CommonFixture();
+            var domainFixture = new ItemCategoryFixture(commonFixture);
+
+            var source = domainFixture.GetItemCategory();
+            var destination = ToDomain.ItemCategoryConverterTests.GetSource(source);
+
+            return (source, destination);
+        }
+
+        protected override void SetupServiceCollection()
+        {
+            serviceCollection.AddInstancesOfGenericType(typeof(ItemCategoryConverter).Assembly, typeof(IToEntityConverter<,>));
+        }
+    }
+}
