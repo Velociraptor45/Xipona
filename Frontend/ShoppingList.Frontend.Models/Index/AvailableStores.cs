e9ce0735b969be4322eeb667a824b60244e06593
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-03-20 10:49:27 +0100

    #85: Update AntDesign to 0.7.2, update WASM, comment out all selects and fix one

--- Frontend/ShoppingList.Frontend.Models/Index/AvailableStores.cs
+++ Frontend/ShoppingList.Frontend.Models/Index/AvailableStores.cs
@@ -6,31 +6,13 @@ namespace ProjectHermes.ShoppingList.Frontend.Models.Index
 {
     public class AvailableStores
     {
-        private readonly IEnumerable<Store> stores;
-        private int selectedStoreIndex;
-
-        public AvailableStores(IEnumerable<Store> stores, int selectedStoreIndex = 0)
+        public AvailableStores(IEnumerable<Store> stores, int selectedStoreId)
         {
-            this.stores = stores ?? throw new ArgumentNullException(nameof(stores));
-
-            if (selectedStoreIndex < 0)
-                throw new ArgumentException($"{nameof(selectedStoreIndex)} mustn't be negative");
-            if (stores.Any() && selectedStoreIndex >= stores.ToList().Count)
-                throw new ArgumentException($"{nameof(selectedStoreIndex)} is bigger than store count");
-
-            this.selectedStoreIndex = selectedStoreIndex;
+            Stores = stores?.ToList() ?? throw new ArgumentNullException(nameof(stores));
+            SelectedStoreId = selectedStoreId;
         }
 
-        public IReadOnlyCollection<Store> Stores => stores.ToList().AsReadOnly();
-        public Store SelectedStore => stores.Any() ? stores.ElementAt(selectedStoreIndex) : null;
-
-        public void SetSelectedStore(int storeId)
-        {
-            int index = stores.ToList().FindIndex(store => store.Id == storeId);
-            if (index == -1)
-                throw new ArgumentException("No store with given id found.");
-
-            selectedStoreIndex = index;
-        }
+        public List<Store> Stores { get; }
+        public int SelectedStoreId { get; set; }
     }
 }
