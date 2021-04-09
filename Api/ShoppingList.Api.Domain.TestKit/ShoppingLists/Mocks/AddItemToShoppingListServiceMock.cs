0ba12d8cc5869ed7d1dd25cf3ba4630d9686942e
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-04-09 12:47:23 +0200

    #79: Implement AddItemToShoppingListServiceMock

---
+++ Api/ShoppingList.Api.Domain.TestKit/ShoppingLists/Mocks/AddItemToShoppingListServiceMock.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using Moq;
+using ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Models;
+using ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Services;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Stores.Models;
+using System.Threading;
+
+namespace ShoppingList.Api.Domain.TestKit.ShoppingLists.Mocks
+{
+    public class AddItemToShoppingListServiceMock
+    {
+        private readonly Mock<IAddItemToShoppingListService> mock;
+
+        public AddItemToShoppingListServiceMock(Fixture fixture)
+        {
+            mock = fixture.Freeze<Mock<IAddItemToShoppingListService>>();
+        }
+
+        public void VerifyAddItemToShoppingListOnce(IShoppingList shoppingList, ItemId itemId, SectionId sectionId,
+            float quantity)
+        {
+            mock.Verify(i => i.AddItemToShoppingList(
+                shoppingList,
+                itemId,
+                sectionId,
+                quantity,
+                It.IsAny<CancellationToken>()));
+        }
+
+        public void VerifyAddItemToShoppingListOnce(IShoppingList shoppingList, TemporaryItemId temporaryItemId,
+            SectionId sectionId, float quantity)
+        {
+            mock.Verify(i => i.AddItemToShoppingList(
+                shoppingList,
+                temporaryItemId,
+                sectionId,
+                quantity,
+                It.IsAny<CancellationToken>()));
+        }
+    }
+}
