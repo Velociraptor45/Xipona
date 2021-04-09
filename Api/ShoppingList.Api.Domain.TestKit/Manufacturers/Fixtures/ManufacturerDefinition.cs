ddbd0e61e55e943ebe98b24ffb9dba644ecd1a1b
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-04-09 10:38:12 +0200

    #79: Implement ManufacturerDefinition

---
+++ Api/ShoppingList.Api.Domain.TestKit/Manufacturers/Fixtures/ManufacturerDefinition.cs
@@ -0,0 +1,9 @@
+﻿using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
+
+namespace ShoppingList.Api.Domain.TestKit.Manufacturers.Fixtures
+{
+    public class ManufacturerDefinition
+    {
+        public ManufacturerId Id { get; set; }
+    }
+}
