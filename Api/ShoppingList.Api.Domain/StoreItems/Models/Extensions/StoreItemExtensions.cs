91b2ece96434003bb15e82019ca09e026896dfae
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-04-18 14:03:49 +0200

    #79: Implement ItemSearchReadModelConversionService

--- Api/ShoppingList.Api.Domain/StoreItems/Models/Extensions/StoreItemExtensions.cs
+++ Api/ShoppingList.Api.Domain/StoreItems/Models/Extensions/StoreItemExtensions.cs
@@ -1,33 +1,9 @@
-﻿using ProjectHermes.ShoppingList.Api.Core.Attributes;
-using ProjectHermes.ShoppingList.Api.Core.Extensions;
-using ProjectHermes.ShoppingList.Api.Domain.Common.Models.Extensions;
-using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models;
-using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
-using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.ItemFilterResults;
-using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.ItemSearch;
-using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.SharedModels;
-using ProjectHermes.ShoppingList.Api.Domain.Stores.Models;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.ItemFilterResults;
 
 namespace ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models.Extensions
 {
     public static class StoreItemExtensions
     {
-        public static ItemSearchReadModel ToItemSearchReadModel(this IStoreItem storeItem, StoreId storeId,
-            IItemCategory itemCategory, IManufacturer manufacturer, IStoreSection defaultSection,
-            IStoreItemAvailability storeAvailability)
-        {
-            return new ItemSearchReadModel(
-                storeItem.Id,
-                storeItem.Name,
-                storeItem.QuantityType.GetAttribute<DefaultQuantityAttribute>().DefaultQuantity,
-                storeAvailability.Price,
-                manufacturer?.ToReadModel(),
-                itemCategory?.ToReadModel(),
-                defaultSection.ToReadModel());
-        }
-
         public static ItemFilterResultReadModel ToItemFilterResultReadModel(this IStoreItem model)
         {
             return new ItemFilterResultReadModel(model.Id, model.Name);
--- Api/ShoppingList.Api.Domain/StoreItems/Queries/ItemSearch/ItemSearchQueryHandler.cs
+++ Api/ShoppingList.Api.Domain/StoreItems/Queries/ItemSearch/ItemSearchQueryHandler.cs
@@ -1,15 +1,12 @@
 ﻿using ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions.Reason;
 using ProjectHermes.ShoppingList.Api.Domain.Common.Queries;
-using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models;
-using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Ports;
-using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
-using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Ports;
 using ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Models;
 using ProjectHermes.ShoppingList.Api.Domain.ShoppingLists.Ports;
 using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models;
-using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models.Extensions;
 using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Ports;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Services.Conversion.ItemSearchReadModels;
+using ProjectHermes.ShoppingList.Api.Domain.Stores.Models;
 using ProjectHermes.ShoppingList.Api.Domain.Stores.Ports;
 using System;
 using System.Collections.Generic;
@@ -24,18 +21,15 @@ namespace ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.ItemSearch
         private readonly IItemRepository itemRepository;
         private readonly IShoppingListRepository shoppingListRepository;
         private readonly IStoreRepository storeRepository;
-        private readonly IItemCategoryRepository itemCategoryRepository;
-        private readonly IManufacturerRepository manufacturerRepository;
+        private readonly IItemSearchReadModelConversionService itemSearchReadModelConversionService;
 
         public ItemSearchQueryHandler(IItemRepository itemRepository, IShoppingListRepository shoppingListRepository,
-            IStoreRepository storeRepository, IItemCategoryRepository itemCategoryRepository,
-            IManufacturerRepository manufacturerRepository)
+            IStoreRepository storeRepository, IItemSearchReadModelConversionService itemSearchReadModelConversionService)
         {
             this.itemRepository = itemRepository;
             this.shoppingListRepository = shoppingListRepository;
             this.storeRepository = storeRepository;
-            this.itemCategoryRepository = itemCategoryRepository;
-            this.manufacturerRepository = manufacturerRepository;
+            this.itemSearchReadModelConversionService = itemSearchReadModelConversionService;
         }
 
         public async Task<IEnumerable<ItemSearchReadModel>> HandleAsync(ItemSearchQuery query, CancellationToken cancellationToken)
@@ -45,7 +39,7 @@ namespace ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.ItemSearch
             if (string.IsNullOrWhiteSpace(query.SearchInput))
                 return Enumerable.Empty<ItemSearchReadModel>();
 
-            var store = await storeRepository.FindByAsync(new Stores.Models.StoreId(query.StoreId.Value), cancellationToken);
+            var store = await storeRepository.FindByAsync(new StoreId(query.StoreId.Value), cancellationToken);
             if (store == null)
                 throw new DomainException(new StoreNotFoundReason(query.StoreId));
 
@@ -58,27 +52,7 @@ namespace ProjectHermes.ShoppingList.Api.Domain.StoreItems.Queries.ItemSearch
             var itemsNotOnShoppingList = storeItems
                 .Where(item => !itemIdsOnShoppingList.Contains(item.Id));
 
-            var itemCategoryIds = storeItems.Select(i => i.ItemCategoryId).Distinct();
-            var itemCategoryDict = (await itemCategoryRepository.FindByAsync(itemCategoryIds, cancellationToken))
-                .ToDictionary(i => i.Id);
-
-            var manufacturerIds = storeItems.Select(i => i.ManufacturerId).Distinct();
-            var manufaturerDict = (await manufacturerRepository.FindByAsync(manufacturerIds, cancellationToken))
-                .ToDictionary(m => m.Id);
-
-            return itemsNotOnShoppingList
-                .Select(item =>
-                {
-                    IManufacturer manufacturer = item.ManufacturerId == null ? null : manufaturerDict[item.ManufacturerId];
-                    IItemCategory category = item.ItemCategoryId == null ? null : itemCategoryDict[item.ItemCategoryId];
-
-                    IStoreItemAvailability storeAvailability = item.Availabilities
-                        .Single(av => av.StoreId == query.StoreId);
-
-                    var section = store.Sections.Single(s => s.Id == storeAvailability.DefaultSectionId);
-
-                    return item.ToItemSearchReadModel(query.StoreId, category, manufacturer, section, storeAvailability);
-                });
+            return await itemSearchReadModelConversionService.ConvertAsync(itemsNotOnShoppingList, store, cancellationToken);
         }
     }
 }
