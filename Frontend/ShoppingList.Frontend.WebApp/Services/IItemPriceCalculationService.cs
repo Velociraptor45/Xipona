76586d7c3eb0db80dbbd08741e4d31c4477853e8
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-06-18 19:17:59 +0200

    #89: Rename Service folder to Services

---
+++ Frontend/ShoppingList.Frontend.WebApp/Services/IItemPriceCalculationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace ProjectHermes.ShoppingList.Frontend.WebApp.Services
+{
+    public interface IItemPriceCalculationService
+    {
+        float CalculatePrice(int quantityTypeId, float pricePerQuantity, float quantity);
+        Task InitializeAsync();
+    }
+}
