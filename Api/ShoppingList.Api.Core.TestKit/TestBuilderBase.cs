e965bc1ee9fba21bab3f22d4ec14a4d9860d1d97
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-08-08 14:56:09 +0200

    #118: Implement StoreItem & StoreItemAvailability Builder and Mother

---
+++ Api/ShoppingList.Api.Core.TestKit/TestBuilderBase.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture;
+using ProjectHermes.ShoppingList.Api.Core.Tests.AutoFixture;
+
+namespace ShoppingList.Api.Core.TestKit
+{
+    public class TestBuilderBase<TModelType> : Fixture
+    {
+        protected void FillContructorWith<TParameter>(string parameterName, TParameter value)
+        {
+            this.ConstructorArgumentFor<TModelType, TParameter>(parameterName, value);
+        }
+
+        public TModelType Create()
+        {
+            return this.Create<TModelType>();
+        }
+    }
+}
---
+++ Api/ShoppingList.Api.Domain.TestKit/StoreItems/Models/StoreItemBuilder.cs
@@ -0,0 +1,82 @@
+﻿using ProjectHermes.ShoppingList.Api.Domain.Common.Models;
+using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
+using ProjectHermes.ShoppingList.Api.Domain.StoreItems.Models;
+using ShoppingList.Api.Core.TestKit;
+using System.Collections.Generic;
+
+namespace ShoppingList.Api.Domain.TestKit.StoreItems.Models
+{
+    public class StoreItemBuilder : TestBuilderBase<StoreItem>
+    {
+        public StoreItemBuilder WithId(ItemId id)
+        {
+            FillContructorWith("id", id);
+            return this;
+        }
+
+        public StoreItemBuilder WithIsDeleted(bool isDeleted)
+        {
+            FillContructorWith("isDeleted", isDeleted);
+            return this;
+        }
+
+        public StoreItemBuilder WithIsTemporary(bool isTemporary)
+        {
+            FillContructorWith("isTemporary", isTemporary);
+            return this;
+        }
+
+        public StoreItemBuilder WithQuantityType(QuantityType quantityType)
+        {
+            FillContructorWith("quantityType", quantityType);
+            return this;
+        }
+
+        public StoreItemBuilder WithQuantityInPacket(float quantityInPacket)
+        {
+            FillContructorWith("quantityInPacket", quantityInPacket);
+            return this;
+        }
+
+        public StoreItemBuilder WithQuantityTypeInPacket(QuantityTypeInPacket quantityTypeInPacket)
+        {
+            FillContructorWith("quantityTypeInPacket", quantityTypeInPacket);
+            return this;
+        }
+
+        public StoreItemBuilder WithItemCategoryId(ItemCategoryId itemCategoryId)
+        {
+            FillContructorWith("itemCategoryId", itemCategoryId);
+            return this;
+        }
+
+        public StoreItemBuilder WithoutItemCategoryId()
+        {
+            return WithItemCategoryId(null);
+        }
+
+        public StoreItemBuilder WithManufacturerId(ManufacturerId manufacturerId)
+        {
+            FillContructorWith("manufacturerId", manufacturerId);
+            return this;
+        }
+
+        public StoreItemBuilder WithoutManufacturerId()
+        {
+            return WithManufacturerId(null);
+        }
+
+        public StoreItemBuilder WithAvailabilities(IEnumerable<IStoreItemAvailability> availabilities)
+        {
+            FillContructorWith("availabilities", availabilities);
+            return this;
+        }
+
+        public StoreItemBuilder WithTemporaryId(TemporaryItemId temporaryId)
+        {
+            FillContructorWith("temporaryId", temporaryId);
+            return this;
+        }
+    }
+}
