229cea8a7bba0ca7699f8096edad4391319994d1
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-08-24 13:43:59 +0200

    #118: Implement replacing autofixture ctor setups

--- Api/ShoppingList.Api.Core/Tests/AutoFixture/AutoFixtureExtensions.cs
+++ Api/ShoppingList.Api.Core/Tests/AutoFixture/AutoFixtureExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using AutoFixture;
+using System.Linq;
 
 namespace ProjectHermes.ShoppingList.Api.Core.Tests.AutoFixture
 {
@@ -7,9 +8,11 @@ namespace ProjectHermes.ShoppingList.Api.Core.Tests.AutoFixture
         public static IFixture ConstructorArgumentFor<TTargetType, TValueType>(this IFixture fixture,
             string paramName, TValueType value)
         {
-            fixture.Customizations.Add(
-               new ConstructorArgumentRelay<TTargetType, TValueType>(paramName, value)
-            );
+            var item = fixture.Customizations.FirstOrDefault(c => c is ConstructorArgumentRelay<TTargetType, TValueType>);
+            if (item != null)
+                fixture.Customizations.Remove(item);
+
+            fixture.Customizations.Add(new ConstructorArgumentRelay<TTargetType, TValueType>(paramName, value));
             return fixture;
         }
     }
