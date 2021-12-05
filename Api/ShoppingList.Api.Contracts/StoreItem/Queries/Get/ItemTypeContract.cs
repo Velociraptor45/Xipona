1458591c1b3b8ce22f08f6518e359168028d118c
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2021-12-05 12:24:45 +0100

    #82: Implement item types in StoreItemContract

---
+++ Api/ShoppingList.Api.Contracts/StoreItem/Queries/Get/ItemTypeContract.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectHermes.ShoppingList.Api.Contracts.StoreItem.Queries.Get
+{
+    public class ItemTypeContract
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<StoreItemAvailabilityContract> Availabilities { get; set; }
+    }
+}
