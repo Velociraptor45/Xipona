096441f589301e91eee355c71fac597e7fb69b04
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-02-05 09:38:44 +0100

    #136: Add GlobalUsing files

---
+++ Api/ShoppingList.Api.Domain.Tests/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Moq;
+global using AutoFixture;
+global using FluentAssertions;
+global using FluentAssertions.Execution;
+global using Xunit;
