ce013154f6f47d90bafa0fb9d225f4cf8d036f8b
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-04-23 14:35:04 +0200

    #189: Split up into fixtures and introduce service scopes

---
+++ Api/ShoppingList.Api.Endpoint.IntegrationsTests/DockerFixture.cs
@@ -0,0 +1,38 @@
+﻿using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Services;
+using System;
+using System.IO;
+
+namespace ShoppingList.Api.Endpoint.IntegrationsTests;
+
+public class DockerFixture : IDisposable
+{
+    private readonly ICompositeService _container;
+
+    public DockerFixture()
+    {
+        var fileDir = Path.Combine(Directory.GetCurrentDirectory(), "docker-compose.yml");
+        _container = new Builder()
+            .UseContainer()
+            .UseCompose()
+            .FromFile(fileDir)
+            .RemoveOrphans()
+            //.WaitForProcess("Database", "mariadb", millisTimeout: 30000)
+            //.WaitForPort("Database", "15906/tcp", 30000)
+            .Build()
+            .Start();
+
+        // wait for DB to initialize
+        Task.Delay(10000).GetAwaiter().GetResult();
+    }
+
+    public const string ConnectionString =
+        $"server=127.0.0.1;port=15906;database={DatabaseName};user id=root;pwd=123root;AllowUserVariables=true;UseAffectedRows=false";
+
+    public const string DatabaseName = "dev-shoppinglist";
+
+    public void Dispose()
+    {
+        _container.Dispose();
+    }
+}
--- Api/ShoppingList.Api.Endpoint.IntegrationsTests/v1/Controllers/StoreControllerIntegrationTests.cs
+++ Api/ShoppingList.Api.Endpoint.IntegrationsTests/v1/Controllers/StoreControllerIntegrationTests.cs
@@ -1,71 +1,51 @@
-﻿using Ductus.FluentDocker.Builders;
-using FluentAssertions;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using ProjectHermes.ShoppingList.Api.ApplicationServices;
 using ProjectHermes.ShoppingList.Api.Contracts.Store.Commands.CreateStore;
-using ProjectHermes.ShoppingList.Api.Domain;
+using ProjectHermes.ShoppingList.Api.Contracts.Store.Commands.UpdateStore;
 using ProjectHermes.ShoppingList.Api.Domain.Stores.Models;
 using ProjectHermes.ShoppingList.Api.Domain.Stores.Ports;
-using ProjectHermes.ShoppingList.Api.Endpoint;
 using ProjectHermes.ShoppingList.Api.Endpoint.v1.Controllers;
-using ProjectHermes.ShoppingList.Api.Infrastructure;
-using ProjectHermes.ShoppingList.Api.Infrastructure.Common.Transactions;
 using ProjectHermes.ShoppingList.Api.Infrastructure.ShoppingLists.Contexts;
 using ProjectHermes.ShoppingList.Api.Infrastructure.Stores.Contexts;
-using System;
-using System.IO;
+using ShoppingList.Api.Domain.TestKit.Stores.Models;
 using Xunit;
 
 namespace ShoppingList.Api.Endpoint.IntegrationsTests.v1.Controllers;
 
-public class StoreControllerIntegrationTests
+public class StoreControllerIntegrationTests : IClassFixture<DockerFixture>
 {
-    [Fact]
-    public async Task Test()
+    private readonly LocalFixture _fixture;
+
+    public StoreControllerIntegrationTests(DockerFixture dockerFixture)
     {
-        var fileDir = Path.Combine(Directory.GetCurrentDirectory(), "docker-compose.yml");
+        _fixture = new LocalFixture(dockerFixture);
+    }
 
-        using (var c = new Builder()
-                  .UseContainer()
-                  .UseCompose()
-                  .FromFile(fileDir)
-                  .RemoveOrphans()
-                  //.WaitForProcess("Database", "mariadb", millisTimeout: 30000)
-                  //.WaitForPort("Database", "15906/tcp", 30000)
-                  .Build()
-                  .Start())
+    [Fact]
+    public async Task CreateStoreAsync()
+    {
+        // Arrange
+        using (var arrangementScope = _fixture.CreateNewServiceScope())
         {
-            await Task.Delay(10000);
+            await _fixture.SetupDatabaseAsync(arrangementScope);
+        }
 
-            var services = new ServiceCollection();
-            services.AddDomain();
-            services.AddEndpointControllers();
-            services.AddInfrastructure("server=127.0.0.1;port=15906;database=dev-shoppinglist;user id=root;pwd=123root;AllowUserVariables=true;UseAffectedRows=false");
-            services.AddApplicationServices();
+        var sut = _fixture.CreateSut();
 
-            var provider = services.BuildServiceProvider();
+        var contract = new CreateStoreContract("MyCoolStore",
+            new List<CreateSectionContract> { new("MyCoolSection", 0, true) });
 
-            var contexts = GetDbContexts(provider);
+        // Act
+        await sut.CreateStoreAsync(contract);
 
-            foreach (var context in contexts)
-            {
-                await context.Database.MigrateAsync();
-                Console.WriteLine($"Context {context.GetType().Name} migrated");
-            }
+        // Assert
+        using (var assertionScope = _fixture.CreateNewServiceScope())
+        {
+            var repo = _fixture.CreateStoreRepository(assertionScope);
 
-            var controller = provider.GetRequiredService<StoreController>();
-
-            var contract = new CreateStoreContract("MyCoolStore",
-                new List<CreateSectionContract> { new("MyCoolSection", 0, true) });
-
-            await controller.CreateStoreAsync(contract);
-
-            var repo = provider.GetRequiredService<IStoreRepository>();
-
-            var generator = provider.GetRequiredService<ITransactionGenerator>();
-
-            using (await generator.GenerateAsync(default))
+            using (await _fixture.CreateTransactionAsync(assertionScope))
             {
                 var stores = await repo.GetAsync(default);
 
@@ -75,12 +55,72 @@ public class StoreControllerIntegrationTests
         }
     }
 
-    private IEnumerable<DbContext> GetDbContexts(IServiceProvider provider)
+    [Fact]
+    public async Task UpdateStoreAsync()
     {
-        yield return provider.GetRequiredService<ShoppingListContext>();
-        //yield return provider.GetRequiredService<ItemCategoryContext>();
-        //yield return provider.GetRequiredService<ManufacturerContext>();
-        //yield return provider.GetRequiredService<ItemContext>();
-        yield return provider.GetRequiredService<StoreContext>();
+        // Arrange
+
+        Store store;
+        using (var arrangementScope = _fixture.CreateNewServiceScope())
+        {
+            await _fixture.SetupDatabaseAsync(arrangementScope);
+
+            using (var t = await _fixture.CreateTransactionAsync(arrangementScope))
+            {
+                var repo = _fixture.CreateStoreRepository(arrangementScope);
+                store = StoreMother.Initial().Create();
+                await repo.StoreAsync(store, default);
+                await t.CommitAsync(default);
+            }
+        }
+
+        var sut = _fixture.CreateSut();
+
+        var contract = new UpdateStoreContract(store.Id.Value, "MyStore", new List<UpdateSectionContract>()
+        {
+            new(store.Sections.First().Id.Value, "mySection", 0, true)
+        });
+
+        // Act
+        var response = await sut.UpdateStoreAsync(contract);
+
+        // Assert
+        using (var assertionScope = _fixture.CreateNewServiceScope())
+        {
+            response.Should().BeOfType<OkResult>();
+
+            using (await _fixture.CreateTransactionAsync(assertionScope))
+            {
+                var repo = _fixture.CreateStoreRepository(assertionScope);
+                var stores = await repo.GetAsync(default);
+
+                stores.Should().HaveCount(1);
+                stores.First().Name.Should().Be(new StoreName("MyStore"));
+            }
+        }
+    }
+
+    private class LocalFixture : DatabaseFixture
+    {
+        public LocalFixture(DockerFixture dockerFixture) : base(dockerFixture)
+        {
+        }
+
+        public StoreController CreateSut()
+        {
+            var scope = CreateNewServiceScope();
+            return scope.ServiceProvider.GetRequiredService<StoreController>();
+        }
+
+        public override IEnumerable<DbContext> GetDbContexts(IServiceScope scope)
+        {
+            yield return scope.ServiceProvider.GetRequiredService<ShoppingListContext>();
+            yield return scope.ServiceProvider.GetRequiredService<StoreContext>();
+        }
+
+        public IStoreRepository CreateStoreRepository(IServiceScope scope)
+        {
+            return scope.ServiceProvider.GetRequiredService<IStoreRepository>();
+        }
     }
 }
