b7ddc16749999067cd5c347a8ebfa59efab5056b
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-04-30 11:18:44 +0200

    #155: Implement disabling button for open TemporaryItemCreator when search input is empty

--- Frontend/ShoppingList.Frontend.Models/ShoppingLists/Models/SearchBar.cs
+++ Frontend/ShoppingList.Frontend.Models/ShoppingLists/Models/SearchBar.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ProjectHermes.ShoppingList.Frontend.Models.ShoppingLists.Models
 {
     public class SearchBar
     {
+        private string _input = "";
+
         public SearchBar()
         {
             ResetInput();
@@ -12,10 +15,20 @@ namespace ProjectHermes.ShoppingList.Frontend.Models.ShoppingLists.Models
             Active = false;
         }
 
-        public string Input { get; set; } = "";
+        public string Input
+        {
+            get => _input;
+            set
+            {
+                _input = value;
+                OnInputChanged?.Invoke(_input);
+            }
+        }
 
         public bool Active { get; set; }
         public IEnumerable<SearchItemForShoppingListResult> Options { get; set; }
+
+        public Action<string> OnInputChanged { get; set; }
 
         public void ResetInput()
         {
