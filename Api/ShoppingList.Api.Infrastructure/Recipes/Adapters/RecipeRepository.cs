9e71b672b45efb7376a5ab880ebb694bd3656721
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-07-23 10:42:59 +0200

    #213: Add recipe entities, context & initial migation

---
+++ Api/ShoppingList.Api.Infrastructure/Recipes/Adapters/RecipeRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProjectHermes.ShoppingList.Api.Domain.Recipes.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Recipes.Ports;
+
+namespace ProjectHermes.ShoppingList.Api.Infrastructure.Recipes.Adapters;
+
+public class RecipeRepository : IRecipeRepository
+{
+    public Task<IRecipe> StoreAsync(IRecipe recipe)
+    {
+        throw new NotImplementedException();
+    }
+}
