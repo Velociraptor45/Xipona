0a38b3e1bd66f988d30b57f271ae5c84bde5937a
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-07-23 12:10:36 +0200

    #213: Implement storing recipes and ToDomain converters

---
+++ Api/ShoppingList.Api.Infrastructure/Recipes/Converters/ToDomain/PreparationStepConverter.cs
@@ -0,0 +1,24 @@
+﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+using ProjectHermes.ShoppingList.Api.Domain.Recipes.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Recipes.Models.Factories;
+using PreparationStep = ProjectHermes.ShoppingList.Api.Infrastructure.Recipes.Entities.PreparationStep;
+
+namespace ProjectHermes.ShoppingList.Api.Infrastructure.Recipes.Converters.ToDomain;
+
+public class PreparationStepConverter : IToDomainConverter<PreparationStep, IPreparationStep>
+{
+    private readonly IPreparationStepFactory _preparationStepFactory;
+
+    public PreparationStepConverter(IPreparationStepFactory preparationStepFactory)
+    {
+        _preparationStepFactory = preparationStepFactory;
+    }
+
+    public IPreparationStep ToDomain(PreparationStep source)
+    {
+        return _preparationStepFactory.Create(
+            new PreparationStepId(source.Id),
+            new PreparationStepInstruction(source.Instruction),
+            source.SortingIndex);
+    }
+}
