030707480e52925d4213b9d16c558758e2af7621
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-08-07 10:44:05 +0200

    #202: Update builders

--- Api/ShoppingList.Api.Domain.TestKit/Recipes/Models/PreparationStepBuilder.cs
+++ Api/ShoppingList.Api.Domain.TestKit/Recipes/Models/PreparationStepBuilder.cs
@@ -1,10 +1,22 @@
-﻿using ProjectHermes.ShoppingList.Api.Domain.Recipes.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Recipes.Models;
 using ProjectHermes.ShoppingList.Api.Domain.TestKit.Common;
 
 namespace ProjectHermes.ShoppingList.Api.Domain.TestKit.Recipes.Models;
 
 public class PreparationStepBuilder : DomainTestBuilderBase<PreparationStep>
 {
+    public PreparationStepBuilder WithId(PreparationStepId id)
+    {
+        FillConstructorWith(nameof(id), id);
+        return this;
+    }
+
+    public PreparationStepBuilder WithInstruction(PreparationStepInstruction instruction)
+    {
+        FillConstructorWith(nameof(instruction), instruction);
+        return this;
+    }
+
     public PreparationStepBuilder WithSortingIndex(int sortingIndex)
     {
         FillConstructorWith(nameof(sortingIndex), sortingIndex);
