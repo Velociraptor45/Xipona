8e975d7002d19fa52945f71aff52ca83b76dc00f
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-10-30 12:17:51 +0100

    #254: Implement first architecture tests

---
+++ Api/ShoppingList.Api.ArchitectureTests/Contracts/ContractsTests.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjectHermes.ShoppingList.Api.ArchitectureTests.Contracts;
+
+public class ContractsTests
+{
+    private readonly ContractsFixture _fixture;
+
+    public ContractsTests()
+    {
+        _fixture = new ContractsFixture();
+    }
+
+    [Fact]
+    public void ProjectDependencies()
+    {
+        var result = Types
+            .InAssembly(_fixture.Assembly)
+            .Should()
+            .OnlyHaveDependenciesOn(
+                "ProjectHermes.ShoppingList.Api.Contracts",
+                "System")
+            .GetResult()
+            .IsSuccessful;
+
+        result.Should().BeTrue();
+    }
+
+    private sealed class ContractsFixture
+    {
+        public Assembly Assembly { get; } = Assembly.Load(new AssemblyName("ShoppingList.Api.Contracts"));
+    }
+}
