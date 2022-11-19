3af04c056465e8f1851c8522313474a24579e84b
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-11-19 17:48:12 +0100

    #233: Remove useless 'pragma warning disable'

--- Api/ShoppingList.Api.ApplicationServices/Common/Queries/IQuery.cs
+++ Api/ShoppingList.Api.ApplicationServices/Common/Queries/IQuery.cs
@@ -1,9 +1,5 @@
 ﻿namespace ProjectHermes.ShoppingList.Api.ApplicationServices.Common.Queries;
 
-#pragma warning disable S2326
-
 public interface IQuery<out T>
 {
 }
-
-#pragma warning restore S2326
