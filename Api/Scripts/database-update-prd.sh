36df6c03483ef44a57c95b97a228ac0db96dae12
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2022-11-26 15:45:00 +0100

    #285: Separate API scripts

--- Api/Scripts/database-update-prd.sh
+++ Api/Scripts/database-update-prd.sh
@@ -1,7 +1,8 @@
-setx ASPNETCORE_ENVIRONMENT "production"
-setx PH_SL_VAULT_USERNAME_FILE "E:\\prd-ph-shoppinglist-vault-api-username.txt"
-setx PH_SL_VAULT_PASSWORD_FILE "E:\\prd-ph-shoppinglist-vault-api-password.txt"
-#setx PH_SL_DB_CONNECTION_STRING_FILE "E:\\prd-ph-shoppinglist-connection-string.txt"
+if [[ "${ASPNETCORE_ENVIRONMENT}" = "development" ]]; then
+  echo "Hmmm, your environment variable is set to development. This doesn't seem right..."
+  read -n 1
+  exit
+fi
 
 cd ../ShoppingList.Api.Repositories/
 
@@ -11,8 +12,3 @@ dotnet ef database update --context ShoppingListContext --no-build
 dotnet ef database update --context ItemContext --no-build
 dotnet ef database update --context StoreContext --no-build
 dotnet ef database update --context RecipeContext --no-build
-
-setx ASPNETCORE_ENVIRONMENT ""
-setx PH_SL_VAULT_USERNAME_FILE ""
-setx PH_SL_VAULT_PASSWORD_FILE ""
-setx PH_SL_DB_CONNECTION_STRING_FILE ""
