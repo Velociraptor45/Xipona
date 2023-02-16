d82ff722e145cb3fdff72a6649613cf8c8a6cd6e
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-02-16 08:08:10 +0100

    #298: Add test tools for frontend

---
+++ Frontend/ShoppingList.Frontend.TestTools/Extensions/GenericExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectHermes.ShoppingList.Frontend.TestTools.Extensions;
+
+public static class GenericExtensions
+{
+    public static bool IsEquivalentTo<T>(this T left, T right)
+    {
+        var comparer = new ObjectsComparer.Comparer<T>();
+        return comparer.Compare(left, right);
+    }
+}
