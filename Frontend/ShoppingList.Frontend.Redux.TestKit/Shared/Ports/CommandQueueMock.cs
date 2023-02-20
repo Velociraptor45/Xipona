b3b02337a9cf30c56e22d178e5c851cb02841629
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-02-20 15:39:19 +0100

    #298: Implement HandleChangeItemQuantityAction tests

--- Frontend/ShoppingList.Frontend.Redux.TestKit/Shared/Ports/CommandQueueMock.cs
+++ Frontend/ShoppingList.Frontend.Redux.TestKit/Shared/Ports/CommandQueueMock.cs
@@ -1,5 +1,8 @@
 ﻿using Moq;
+using Moq.Contrib.InOrder.Extensions;
 using ProjectHermes.ShoppingList.Frontend.Redux.Shared.Ports;
+using ProjectHermes.ShoppingList.Frontend.Redux.Shared.Ports.Requests;
+using ProjectHermes.ShoppingList.Frontend.TestTools.Extensions;
 
 namespace ProjectHermes.ShoppingList.Frontend.Redux.TestKit.Shared.Ports;
 
@@ -7,5 +10,26 @@ public class CommandQueueMock : Mock<ICommandQueue>
 {
     public CommandQueueMock(MockBehavior behavior) : base(behavior)
     {
+    }
+
+    public void SetupEnqueue(IApiRequest request)
+    {
+        this.SetupInOrder(m =>
+                m.Enqueue(It.Is<IApiRequest>(r => r.IsRequestEquivalentTo(request))))
+            .Returns(Task.CompletedTask);
+    }
+
+    public void VerifyEnqueue(IApiRequest request, Func<Times> times)
+    {
+        Verify(m =>
+                m.Enqueue(It.Is<IApiRequest>(r => r.IsRequestEquivalentTo(request))),
+                times);
+    }
+
+    public void VerifyNoEnqueue<TRequest>() where TRequest : IApiRequest
+    {
+        Verify(m =>
+                m.Enqueue(It.Is<IApiRequest>(r => r.GetType() == typeof(TRequest))),
+                Times.Never);
     }
 }
---
+++ Frontend/ShoppingList.Frontend.Redux.TestKit/Shared/Services/NavigationManagerMock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Moq;
+using Moq.Contrib.InOrder.Extensions;
+
+namespace ProjectHermes.ShoppingList.Frontend.Redux.TestKit.Shared.Services;
+
+public class NavigationManagerMock : Mock<NavigationManager>
+{
+    public NavigationManagerMock(MockBehavior behavior) : base(behavior)
+    {
+    }
+
+    public void SetupNavigateTo(string uri)
+    {
+        this.SetupInOrder(m => m.NavigateTo(uri, false, false));
+    }
+}
