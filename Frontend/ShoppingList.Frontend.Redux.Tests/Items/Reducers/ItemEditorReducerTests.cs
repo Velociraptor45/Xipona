d4b4b5a7d094ccf8f66b0cf7d8e2a6f2e952a332
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-02-21 10:51:51 +0100

    #298: Implement OnItemNameChanged tests

---
+++ Frontend/ShoppingList.Frontend.Redux.Tests/Items/Reducers/ItemEditorReducerTests.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using ProjectHermes.ShoppingList.Frontend.Redux.Items.Actions.Editor;
+using ProjectHermes.ShoppingList.Frontend.Redux.Items.Reducers;
+using ProjectHermes.ShoppingList.Frontend.Redux.Items.States;
+using ProjectHermes.ShoppingList.Frontend.Redux.TestKit.Common;
+using ProjectHermes.ShoppingList.Frontend.TestTools.Exceptions;
+
+namespace ProjectHermes.ShoppingList.Frontend.Redux.Tests.Items.Reducers;
+
+public class ItemEditorReducerTests
+{
+    public class OnItemNameChanged
+    {
+        private OnItemNameChangedFixture _fixture;
+
+        public OnItemNameChanged()
+        {
+            _fixture = new OnItemNameChangedFixture();
+        }
+
+        [Fact]
+        public void OnItemNameChanged_WithValidName_ShouldSetName()
+        {
+            // Arrange
+            _fixture.SetupActionWithName();
+            _fixture.SetupInitialState();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.Action);
+
+            // Act
+            var result = ItemEditorReducer.OnItemNameChanged(_fixture.InitialState, _fixture.Action);
+
+            // Assert
+            result.Should().BeEquivalentTo(_fixture.ExpectedState);
+        }
+
+        [Fact]
+        public void OnItemNameChanged_WithNameNull_ShouldSetName()
+        {
+            // Arrange
+            _fixture.SetupActionWithoutName();
+            _fixture.SetupInitialState();
+            _fixture.SetupExpectedResultWithEmptyName();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.Action);
+
+            // Act
+            var result = ItemEditorReducer.OnItemNameChanged(_fixture.InitialState, _fixture.Action);
+
+            // Assert
+            result.Should().BeEquivalentTo(_fixture.ExpectedState);
+        }
+
+        private sealed class OnItemNameChangedFixture : ItemEditorReducerFixture
+        {
+            public ItemNameChangedAction? Action { get; private set; }
+
+            public void SetupActionWithName()
+            {
+                Action = new DomainTestBuilder<ItemNameChangedAction>().Create() with
+                {
+                    Name = ExpectedState.Editor.Item!.Name
+                };
+            }
+
+            public void SetupActionWithoutName()
+            {
+                Action = new DomainTestBuilder<ItemNameChangedAction>().Create() with
+                {
+                    Name = null
+                };
+            }
+
+            public void SetupExpectedResultWithEmptyName()
+            {
+                ExpectedState = ExpectedState with
+                {
+                    Editor = ExpectedState.Editor with
+                    {
+                        Item = ExpectedState.Editor.Item! with
+                        {
+                            Name = string.Empty
+                        }
+                    }
+                };
+            }
+
+            public void SetupInitialState()
+            {
+                InitialState = ExpectedState with
+                {
+                    Editor = ExpectedState.Editor with
+                    {
+                        Item = ExpectedState.Editor.Item! with
+                        {
+                            Name = new DomainTestBuilder<string>().Create()
+                        }
+                    }
+                };
+            }
+        }
+    }
+
+    private abstract class ItemEditorReducerFixture
+    {
+        public ItemState ExpectedState { get; protected set; } = new DomainTestBuilder<ItemState>().Create();
+        public ItemState InitialState { get; protected set; } = new DomainTestBuilder<ItemState>().Create();
+    }
+}
