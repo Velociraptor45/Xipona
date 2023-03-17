3410f5cd185d55b8c71938ff086842fe9c22b5da
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-03-17 19:06:27 +0100

    #298: Implement OnSectionDecremented tests

--- Frontend/ShoppingList.Frontend.Redux.Tests/Stores/Reducers/StoreEditorReducerTests.cs
+++ Frontend/ShoppingList.Frontend.Redux.Tests/Stores/Reducers/StoreEditorReducerTests.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
+using ProjectHermes.ShoppingList.Frontend.Redux.ShoppingList.States.Comparer;
 using ProjectHermes.ShoppingList.Frontend.Redux.Stores.Actions.Editor;
+using ProjectHermes.ShoppingList.Frontend.Redux.Stores.Actions.Editor.Sections;
 using ProjectHermes.ShoppingList.Frontend.Redux.Stores.Reducers;
 using ProjectHermes.ShoppingList.Frontend.Redux.Stores.States;
 using ProjectHermes.ShoppingList.Frontend.Redux.TestKit.Common;
@@ -166,6 +168,143 @@ public class StoreEditorReducerTests
             public void SetupActionForStoreNull()
             {
                 Action = new DomainTestBuilder<StoreNameChangedAction>().Create();
+            }
+        }
+    }
+
+    public class OnSectionDecremented
+    {
+        private readonly OnSectionDecrementedFixture _fixture;
+
+        public OnSectionDecremented()
+        {
+            _fixture = new OnSectionDecrementedFixture();
+        }
+
+        [Fact]
+        public void OnSectionDecremented_WithSectionDecrementable_ShouldDecrementSection()
+        {
+            // Arrange
+            _fixture.SetupInitialStateForDecrementingLastSection();
+            _fixture.SetupActionForDecrementingLastSection();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.Action);
+
+            // Act
+            var result = StoreEditorReducer.OnSectionDecremented(_fixture.InitialState, _fixture.Action);
+
+            // Assert
+            result.Should().BeEquivalentTo(_fixture.ExpectedState);
+        }
+
+        [Fact]
+        public void OnSectionDecremented_WithSectionNotDecrementable_ShouldNotChangeSectionOrder()
+        {
+            // Arrange
+            _fixture.SetupInitialStateEqualsExpectedState();
+            _fixture.SetupActionForDecrementingFirstSection();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.Action);
+
+            // Act
+            var result = StoreEditorReducer.OnSectionDecremented(_fixture.InitialState, _fixture.Action);
+
+            // Assert
+            result.Should().BeEquivalentTo(_fixture.ExpectedState);
+        }
+
+        [Fact]
+        public void OnSectionDecremented_WithInvalidSectionKey_ShouldNotChangeSectionOrder()
+        {
+            // Arrange
+            _fixture.SetupInitialStateEqualsExpectedState();
+            _fixture.SetupActionForInvalidSectionKey();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.Action);
+
+            // Act
+            var result = StoreEditorReducer.OnSectionDecremented(_fixture.InitialState, _fixture.Action);
+
+            // Assert
+            result.Should().BeEquivalentTo(_fixture.ExpectedState);
+        }
+
+        [Fact]
+        public void OnSectionDecremented_WithStoreNull_ShouldNotChangeSectionOrder()
+        {
+            // Arrange
+            _fixture.SetupExpectedStateForStoreNull();
+            _fixture.SetupInitialStateEqualsExpectedState();
+            _fixture.SetupActionForStoreNull();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.Action);
+
+            // Act
+            var result = StoreEditorReducer.OnSectionDecremented(_fixture.InitialState, _fixture.Action);
+
+            // Assert
+            result.Should().BeEquivalentTo(_fixture.ExpectedState);
+        }
+
+        private sealed class OnSectionDecrementedFixture : StoreEditorReducerFixture
+        {
+            public SectionDecrementedAction? Action { get; private set; }
+
+            public void SetupInitialStateEqualsExpectedState()
+            {
+                InitialState = ExpectedState;
+            }
+
+            public void SetupInitialStateForDecrementingLastSection()
+            {
+                var sections = ExpectedState.Editor.Store!.Sections.ToList();
+                var sortingIndexTmp = sections[1].SortingIndex;
+                sections[1] = sections[1] with { SortingIndex = sections[2].SortingIndex };
+                sections[2] = sections[2] with { SortingIndex = sortingIndexTmp };
+
+                InitialState = ExpectedState with
+                {
+                    Editor = ExpectedState.Editor with
+                    {
+                        Store = ExpectedState.Editor.Store! with
+                        {
+                            Sections = new SortedSet<EditedSection>(sections, new SortingIndexComparer())
+                        }
+                    }
+                };
+            }
+
+            public void SetupExpectedStateForStoreNull()
+            {
+                ExpectedState = ExpectedState with
+                {
+                    Editor = ExpectedState.Editor with
+                    {
+                        Store = null
+                    }
+                };
+            }
+
+            public void SetupActionForDecrementingLastSection()
+            {
+                var section = InitialState.Editor.Store!.Sections.Last();
+                Action = new SectionDecrementedAction(section.Key);
+            }
+
+            public void SetupActionForDecrementingFirstSection()
+            {
+                var section = InitialState.Editor.Store!.Sections.First();
+                Action = new SectionDecrementedAction(section.Key);
+            }
+
+            public void SetupActionForInvalidSectionKey()
+            {
+                Action = new SectionDecrementedAction(Guid.NewGuid());
+            }
+
+            public void SetupActionForStoreNull()
+            {
+                Action = new DomainTestBuilder<SectionDecrementedAction>().Create();
             }
         }
     }
