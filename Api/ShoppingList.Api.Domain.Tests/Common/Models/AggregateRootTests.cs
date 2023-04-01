e6fb65b728d26ac3b7f5f994cd67efb4cac41c29
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-04-01 13:45:58 +0200

    #276: Implement AggregateRoot tests

---
+++ Api/ShoppingList.Api.Domain.Tests/Common/Models/AggregateRootTests.cs
@@ -0,0 +1,71 @@
+﻿using ProjectHermes.ShoppingList.Api.Domain.Common.Models;
+using ProjectHermes.ShoppingList.Api.Domain.TestKit.Common;
+using ProjectHermes.ShoppingList.Api.TestTools.Exceptions;
+
+namespace ProjectHermes.ShoppingList.Api.Domain.Tests.Common.Models;
+
+public class AggregateRootTests
+{
+    public class EnrichWithRowVersion
+    {
+        private readonly EnrichWithRowVersionFixture _fixture;
+
+        public EnrichWithRowVersion()
+        {
+            _fixture = new EnrichWithRowVersionFixture();
+        }
+
+        [Fact]
+        public void EnrichWithRowVersion_WithoutExistingRowVersion_ShouldSetRowVersion()
+        {
+            // Arrange
+            var sut = _fixture.CreateSut();
+            _fixture.SetupRowVersion();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.RowVersion);
+
+            // Act
+            sut.EnrichWithRowVersion(_fixture.RowVersion);
+
+            // Assert
+            sut.RowVersion.Should().BeEquivalentTo(_fixture.RowVersion);
+        }
+
+        [Fact]
+        public void EnrichWithRowVersion_WithExistingRowVersion_ShouldThrowException()
+        {
+            // Arrange
+            var sut = _fixture.CreateSut();
+            _fixture.SetupRowVersion();
+
+            TestPropertyNotSetException.ThrowIfNull(_fixture.RowVersion);
+
+            sut.EnrichWithRowVersion(_fixture.RowVersion);
+
+            // Act
+            Action act = () => sut.EnrichWithRowVersion(_fixture.RowVersion);
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        private sealed class EnrichWithRowVersionFixture
+        {
+            public byte[]? RowVersion { get; private set; }
+
+            public void SetupRowVersion()
+            {
+                RowVersion = new DomainTestBuilder<byte[]>().Create();
+            }
+
+            public TestAggregateRoot CreateSut()
+            {
+                return new TestAggregateRoot();
+            }
+        }
+    }
+
+    private class TestAggregateRoot : AggregateRoot
+    {
+    }
+}
