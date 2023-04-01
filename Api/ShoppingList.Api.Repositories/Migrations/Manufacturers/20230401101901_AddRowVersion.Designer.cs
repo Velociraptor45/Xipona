1a87eb39a796c834d3345ee068cbbe09b27390e7
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-04-01 12:19:38 +0200

    #276: Add row version to Manufacturer entity

--- Api/ShoppingList.Api.Repositories/Migrations/Manufacturers/20230401101901_AddRowVersion.Designer.cs
+++ Api/ShoppingList.Api.Repositories/Migrations/Manufacturers/20230401101901_AddRowVersion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectHermes.ShoppingList.Api.Repositories.Manufacturers.Contexts;
 
@@ -10,9 +11,11 @@ using ProjectHermes.ShoppingList.Api.Repositories.Manufacturers.Contexts;
 namespace ProjectHermes.ShoppingList.Api.Repositories.Migrations.Manufacturers
 {
     [DbContext(typeof(ManufacturerContext))]
-    partial class ManufacturerContextModelSnapshot : ModelSnapshot
+    [Migration("20230401101901_AddRowVersion")]
+    partial class AddRowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -32,7 +35,7 @@ namespace ProjectHermes.ShoppingList.Api.Repositories.Migrations.Manufacturers
                         .HasColumnType("longtext");
 
                     b.Property<DateTime>("RowVersion")
-                        .IsRowVersion()
+                        .IsConcurrencyToken()
                         .ValueGeneratedOnAddOrUpdate()
                         .HasColumnType("timestamp(6)");
 
