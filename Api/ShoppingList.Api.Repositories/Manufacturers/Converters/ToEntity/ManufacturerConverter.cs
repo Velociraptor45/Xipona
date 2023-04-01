0f607cbe75d021097165b1f160722c3d87a0f3c2
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-04-01 12:31:02 +0200

    #276: Implement Manufacturer inheriting from AggregateRoot

--- Api/ShoppingList.Api.Repositories/Manufacturers/Converters/ToEntity/ManufacturerConverter.cs
+++ Api/ShoppingList.Api.Repositories/Manufacturers/Converters/ToEntity/ManufacturerConverter.cs
@@ -1,4 +1,5 @@
 ﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+using ProjectHermes.ShoppingList.Api.Domain.Common.Models;
 using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
 
 namespace ProjectHermes.ShoppingList.Api.Repositories.Manufacturers.Converters.ToEntity;
@@ -11,7 +12,8 @@ public class ManufacturerConverter : IToEntityConverter<IManufacturer, Entities.
         {
             Id = source.Id,
             Name = source.Name,
-            Deleted = source.IsDeleted
+            Deleted = source.IsDeleted,
+            RowVersion = ((AggregateRoot)source).RowVersion
         };
     }
 }
