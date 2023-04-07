9e57e0b368e3bd8eee0512579fa88c07c9a9e5cf
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-04-07 10:15:43 +0200

    #341: Create TagsForRecipe mapping entity

--- Api/ShoppingList.Api.Repositories/Recipes/Contexts/RecipeContext.cs
+++ Api/ShoppingList.Api.Repositories/Recipes/Contexts/RecipeContext.cs
@@ -5,14 +5,18 @@ namespace ProjectHermes.ShoppingList.Api.Repositories.Recipes.Contexts;
 
 public class RecipeContext : DbContext
 {
-    public DbSet<Recipe> Recipes { get; set; }
-    public DbSet<Ingredient> Ingredients { get; set; }
-    public DbSet<PreparationStep> PreparationSteps { get; set; }
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public DbSet<Recipe> Recipes { get; set; } = null!;
+    public DbSet<Ingredient> Ingredients { get; set; } = null!;
+    public DbSet<PreparationStep> PreparationSteps { get; set; } = null!;
+    public DbSet<TagsForRecipe> Tags { get; set; } = null!;
 
     public RecipeContext(DbContextOptions<RecipeContext> options) : base(options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TagsForRecipe>().HasKey(t => new { t.RecipeId, t.RecipeTagId });
     }
 }
