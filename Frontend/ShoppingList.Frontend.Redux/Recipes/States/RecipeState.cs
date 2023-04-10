96ab809d9dbc24c19991fc0df13780ef44dd80e8
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-04-10 16:07:37 +0200

    #341: Style recipe tag select

--- Frontend/ShoppingList.Frontend.Redux/Recipes/States/RecipeState.cs
+++ Frontend/ShoppingList.Frontend.Redux/Recipes/States/RecipeState.cs
@@ -6,7 +6,19 @@ public record RecipeState(
     IReadOnlyCollection<IngredientQuantityType> IngredientQuantityTypes,
     IReadOnlyCollection<RecipeTag> RecipeTags,
     RecipeSearch Search,
-    RecipeEditor Editor);
+    RecipeEditor Editor)
+{
+    public IEnumerable<string> GetTagNamesFor(IEnumerable<Guid> recipeTagIds)
+    {
+        var tagDict = RecipeTags.ToDictionary(x => x.Id, x => x.Name);
+
+        foreach (var recipeTagId in recipeTagIds)
+        {
+            if (tagDict.TryGetValue(recipeTagId, out var tagName))
+                yield return tagName;
+        }
+    }
+}
 
 public class RecipeFeatureState : Feature<RecipeState>
 {
