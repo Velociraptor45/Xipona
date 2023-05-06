293cb48bf8df8ebc6f3fe992b876e3bfc2c29005
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-05-06 15:22:52 +0200

    #54: Add base converter tests

---
+++ Api/ShoppingList.Api.Core.TestKit/Common/ToContractConverterBase.cs
@@ -0,0 +1,50 @@
+﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+
+namespace ProjectHermes.ShoppingList.Api.Core.TestKit.Common;
+
+public abstract class ToContractConverterBase<TSource, TDest, TConverter> where TConverter : IToContractConverter<TSource, TDest>
+{
+    [Fact]
+    public void Convert_ShouldMapAllMembersCorrectly()
+    {
+        // Arrange
+        var builder = new TestBuilder<TSource>();
+        Customize(builder);
+        var contract = builder.Create();
+
+        var mapper = new MapperConfiguration(cfg =>
+        {
+            AddMapping(cfg);
+            AddAdditionalMapping(cfg);
+        }).CreateMapper();
+
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+        var expectedResult = mapper.Map<TDest>(contract);
+        var sut = CreateSut();
+
+        // Act
+        var result = sut.ToContract(contract);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    protected abstract TConverter CreateSut();
+
+    protected virtual void AddAdditionalMapping(IMapperConfigurationExpression cfg)
+    {
+    }
+
+    protected abstract void AddMapping(IMappingExpression<TSource, TDest> mapping);
+
+    protected virtual void Customize(IFixture fixture)
+    {
+    }
+
+    public void AddMapping(IMapperConfigurationExpression cfg)
+    {
+        var mapping = cfg.CreateMap<TSource, TDest>(MemberList.Destination);
+        AddMapping(mapping);
+    }
+}
---
+++ Api/ShoppingList.Api.Core.TestKit/Common/ToDomainConverterBase.cs
@@ -0,0 +1,50 @@
+﻿using ProjectHermes.ShoppingList.Api.Core.Converter;
+
+namespace ProjectHermes.ShoppingList.Api.Core.TestKit.Common;
+
+public abstract class ToDomainConverterBase<TSource, TDest, TConverter> where TConverter : IToDomainConverter<TSource, TDest>
+{
+    [Fact]
+    public void Convert_ShouldMapAllMembersCorrectly()
+    {
+        // Arrange
+        var builder = new TestBuilder<TSource>();
+        Customize(builder);
+        var contract = builder.Create();
+
+        var mapper = new MapperConfiguration(cfg =>
+        {
+            AddMapping(cfg);
+            AddAdditionalMapping(cfg);
+        }).CreateMapper();
+
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+        var expectedResult = mapper.Map<TDest>(contract);
+        var sut = CreateSut();
+
+        // Act
+        var result = sut.ToDomain(contract);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    protected abstract TConverter CreateSut();
+
+    protected virtual void AddAdditionalMapping(IMapperConfigurationExpression cfg)
+    {
+    }
+
+    protected abstract void AddMapping(IMappingExpression<TSource, TDest> mapping);
+
+    protected virtual void Customize(IFixture fixture)
+    {
+    }
+
+    public void AddMapping(IMapperConfigurationExpression cfg)
+    {
+        var mapping = cfg.CreateMap<TSource, TDest>(MemberList.Destination);
+        AddMapping(mapping);
+    }
+}
