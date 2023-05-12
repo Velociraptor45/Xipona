c6d91422e0f5fe50468c6a09c5845924c67fbcbf
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-05-12 20:23:03 +0200

    #54: Implement ToDomain converters for Store & Section entities

---
+++ Api/ShoppingList.Api.Repositories.Tests/Stores/Converters/ToDomain/SectionConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using ProjectHermes.ShoppingList.Api.Core.Tests.Converter;
+using ProjectHermes.ShoppingList.Api.Domain.Stores.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Stores.Models.Factories;
+using ProjectHermes.ShoppingList.Api.Repositories.Stores.Converters.ToDomain;
+using ProjectHermes.ShoppingList.Api.Repositories.TestKit.Stores.Entities;
+using Section = ProjectHermes.ShoppingList.Api.Repositories.Stores.Entities.Section;
+
+namespace ProjectHermes.ShoppingList.Api.Repositories.Tests.Stores.Converters.ToDomain;
+
+public class SectionConverterTests : ToDomainConverterBase<Section, ISection, SectionConverter>
+{
+    protected override Section CreateSource()
+    {
+        return new SectionEntityBuilder().Create();
+    }
+
+    public override SectionConverter CreateSut()
+    {
+        return new SectionConverter(new SectionFactory());
+    }
+
+    protected override void AddMapping(IMappingExpression<Section, ISection> mapping)
+    {
+        mapping.As<Domain.Stores.Models.Section>();
+    }
+
+    protected override void AddAdditionalMapping(IMapperConfigurationExpression cfg)
+    {
+        cfg.CreateMap<Section, Domain.Stores.Models.Section>()
+            .ForCtorParam(nameof(ISection.Id), opt => opt.MapFrom(src => new SectionId(src.Id)))
+            .ForCtorParam(nameof(ISection.Name), opt => opt.MapFrom(src => new SectionName(src.Name)))
+            .ForCtorParam(nameof(ISection.SortingIndex), opt => opt.MapFrom(src => src.SortIndex))
+            .ForCtorParam(nameof(ISection.IsDefaultSection), opt => opt.MapFrom(src => src.IsDefaultSection))
+            .ForCtorParam(nameof(ISection.IsDeleted), opt => opt.MapFrom(src => src.IsDeleted));
+    }
+}
