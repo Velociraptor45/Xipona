df8aefd62bfe878fdeee8b0ad51b737543bf3d66
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-05-19 20:48:22 +0200

    #54: Implement item category and manufacturer converter tests

---
+++ Api/ShoppingList.Api.Repositories.Tests/ItemCategories/Converters/ToContract/ItemCategoryConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ProjectHermes.ShoppingList.Api.Core.Tests.Converter;
+using ProjectHermes.ShoppingList.Api.Domain.Common.Models;
+using ProjectHermes.ShoppingList.Api.Domain.ItemCategories.Models;
+using ProjectHermes.ShoppingList.Api.Repositories.ItemCategories.Converters.ToContract;
+using ItemCategory = ProjectHermes.ShoppingList.Api.Repositories.ItemCategories.Entities.ItemCategory;
+
+namespace ProjectHermes.ShoppingList.Api.Repositories.Tests.ItemCategories.Converters.ToContract;
+
+public class ItemCategoryConverterTests : ToContractConverterBase<IItemCategory, ItemCategory, ItemCategoryConverter>
+{
+    protected override ItemCategoryConverter CreateSut()
+    {
+        return new();
+    }
+
+    protected override void AddMapping(IMappingExpression<IItemCategory, ItemCategory> mapping)
+    {
+        mapping
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.Value))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.Value))
+            .ForMember(dest => dest.Deleted, opt => opt.MapFrom(src => src.IsDeleted))
+            .ForMember(dest => dest.RowVersion, opt => opt.MapFrom(src => ((AggregateRoot)src).RowVersion));
+    }
+}
---
+++ Api/ShoppingList.Api.Repositories.Tests/Manufacturers/Converters/ToDomain/ManufacturerConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ProjectHermes.ShoppingList.Api.Core.Tests.Converter;
+using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models;
+using ProjectHermes.ShoppingList.Api.Domain.Manufacturers.Models.Factories;
+using ProjectHermes.ShoppingList.Api.Repositories.Manufacturers.Converters.ToDomain;
+using ProjectHermes.ShoppingList.Api.TestTools.Extensions;
+using Manufacturer = ProjectHermes.ShoppingList.Api.Repositories.Manufacturers.Entities.Manufacturer;
+
+namespace ProjectHermes.ShoppingList.Api.Repositories.Tests.Manufacturers.Converters.ToDomain;
+
+public class ManufacturerConverterTests : ToDomainConverterBase<Manufacturer, IManufacturer, ManufacturerConverter>
+{
+    public override ManufacturerConverter CreateSut()
+    {
+        return new(new ManufacturerFactory());
+    }
+
+    protected override void AddMapping(IMappingExpression<Manufacturer, IManufacturer> mapping)
+    {
+        mapping.As<Domain.Manufacturers.Models.Manufacturer>();
+    }
+
+    protected override void AddAdditionalMapping(IMapperConfigurationExpression cfg)
+    {
+        cfg.CreateMap<Manufacturer, Domain.Manufacturers.Models.Manufacturer>()
+            .ForCtorParam(nameof(IManufacturer.Id).LowerFirstChar(), opt => opt.MapFrom(src => new ManufacturerId(src.Id)))
+            .ForCtorParam(nameof(IManufacturer.Name).LowerFirstChar(), opt => opt.MapFrom(src => new ManufacturerName(src.Name)))
+            .ForCtorParam(nameof(IManufacturer.IsDeleted).LowerFirstChar(), opt => opt.MapFrom(src => src.Deleted))
+            .ForMember(dest => dest.RowVersion, opt => opt.MapFrom(src => src.RowVersion))
+            .ForMember(dest => dest.DomainEvents, opt => opt.Ignore());
+    }
+}
