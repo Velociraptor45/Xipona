a835439812003c774e55fbfc8369849dde9651dc
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-08-27 09:09:21 +0200

    #404: Convert PreparationStepInstruction to record

--- Api/ShoppingList.Api.Domain/Recipes/Models/PreparationStepInstruction.cs
+++ Api/ShoppingList.Api.Domain/Recipes/Models/PreparationStepInstruction.cs
@@ -1,11 +1,3 @@
 ﻿namespace ProjectHermes.ShoppingList.Api.Domain.Recipes.Models;
 
-public class PreparationStepInstruction
-{
-    public PreparationStepInstruction(string value)
-    {
-        Value = value;
-    }
-
-    public string Value { get; }
-}
+public record PreparationStepInstruction(string Value);
