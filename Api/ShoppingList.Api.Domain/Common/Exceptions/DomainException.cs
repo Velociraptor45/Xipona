179af91058accf4176a05695325953ecb2b02658
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-02-03 10:27:21 +0100

    #455: Remove obsolete serialization

--- Api/ShoppingList.Api.Domain/Common/Exceptions/DomainException.cs
+++ Api/ShoppingList.Api.Domain/Common/Exceptions/DomainException.cs
@@ -1,27 +1,8 @@
 ﻿using ProjectHermes.ShoppingList.Api.Domain.Common.Reasons;
-using System.Runtime.Serialization;
 
 namespace ProjectHermes.ShoppingList.Api.Domain.Common.Exceptions;
 
-[Serializable]
-public class DomainException : Exception
+public class DomainException(IReason reason) : Exception
 {
-    public DomainException(IReason reason)
-    {
-        Reason = reason;
-    }
-
-    protected DomainException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
-    {
-        Reason = (IReason)info.GetValue("Reason", typeof(IReason))!;
-    }
-
-    public override void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-        base.GetObjectData(info, context);
-        info.AddValue("Reason", Reason);
-    }
-
-    public IReason Reason { get; }
+    public IReason Reason { get; } = reason;
 }
