67caf1bff7000a5500e2b60328d6b72a87a4b4cf
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-03-02 08:25:14 +0100

    #423: Add manufacturer name to item search results in UI

--- Frontend/ShoppingList.Frontend.Redux/Items/States/ItemSearchResult.cs
+++ Frontend/ShoppingList.Frontend.Redux/Items/States/ItemSearchResult.cs
@@ -4,12 +4,14 @@ namespace ProjectHermes.ShoppingList.Frontend.Redux.Items.States;
 
 public class ItemSearchResult : ISearchResult
 {
-    public ItemSearchResult(Guid id, string name)
+    public ItemSearchResult(Guid id, string name, string manufacturerName)
     {
         Id = id;
         Name = name;
+        ManufacturerName = manufacturerName;
     }
 
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public string ManufacturerName { get; set; }
 }
