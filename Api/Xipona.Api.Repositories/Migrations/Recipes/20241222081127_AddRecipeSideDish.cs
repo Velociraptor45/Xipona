1aba64ddea3058b34a0973dbe8c8afba6a6ba63d
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-12-22 09:52:34 +0100

    #467: Add side dish DB migration

---
+++ Api/Xipona.Api.Repositories/Migrations/Recipes/20241222081127_AddRecipeSideDish.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectHermes.Xipona.Api.Repositories.Migrations.Recipes
+{
+    /// <inheritdoc />
+    public partial class AddRecipeSideDish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SideDishId",
+                table: "Recipes",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_SideDishId",
+                table: "Recipes",
+                column: "SideDishId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Recipes_SideDishId",
+                table: "Recipes",
+                column: "SideDishId",
+                principalTable: "Recipes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Recipes_SideDishId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_SideDishId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "SideDishId",
+                table: "Recipes");
+        }
+    }
+}
