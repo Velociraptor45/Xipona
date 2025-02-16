f5e5d2a6d3c5a54303c8bf8885a4bc47515f173b
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2025-02-16 13:26:03 +0100

    #530: Remove unused code

--- Api/Xipona.Api.Endpoint/ServiceCollectionExtensions.cs
+++ Api/Xipona.Api.Endpoint/ServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ProjectHermes.Xipona.Api.Core.Converter;
 using ProjectHermes.Xipona.Api.Core.Extensions;
-using ProjectHermes.Xipona.Api.Endpoint.v1.Converters;
-using System.Reflection;
 
 namespace ProjectHermes.Xipona.Api.Endpoint;
 
@@ -13,41 +11,5 @@ public static class ServiceCollectionExtensions
         var assembly = typeof(ServiceCollectionExtensions).Assembly;
         services.AddImplementationOfGenericType(assembly, typeof(IToContractConverter<,>));
         services.AddImplementationOfGenericType(assembly, typeof(IToDomainConverter<,>));
-
-        services.AddSingleton<IEndpointConverters>(provider =>
-        {
-            var toContract = new EndpointToContractConverters();
-            var toDomain = new EndpointToDomainConverters();
-
-            AddConverters(provider, toContract, Assembly.GetExecutingAssembly(), typeof(IToContractConverter<,>));
-            AddConverters(provider, toDomain, Assembly.GetExecutingAssembly(), typeof(IToDomainConverter<,>));
-            return new EndpointConverters(toDomain, toContract);
-        });
-    }
-
-    private static void AddConverters<TConverter>(IServiceProvider provider,
-        Dictionary<(Type, Type), TConverter> dict, Assembly assembly, Type type)
-        where TConverter : class, IConverter
-    {
-        var assemblyTypes = assembly
-            .GetTypes()
-            .Where(t => !t.IsAbstract)
-            .ToList();
-        foreach (var assemblyType in assemblyTypes)
-        {
-            var interfaceTypes = assemblyType
-                .GetInterfaces()
-                .Where(t => t.IsGenericType
-                                      && t.GetGenericTypeDefinition() == type);
-
-            foreach (var interfaceType in interfaceTypes)
-            {
-                var args = interfaceType.GetGenericArguments();
-                if (provider.GetRequiredService(interfaceType) is not TConverter implementation || args.Length != 2)
-                    continue;
-
-                dict.Add((args[0], args[1]), implementation);
-            }
-        }
     }
 }
