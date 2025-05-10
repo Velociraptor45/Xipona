ab6d4b53b558dcf83e980002ad8b9a211ae2930f
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2025-05-10 08:14:18 +0200

    #528: Implement effects tests

---
+++ Frontend/Xipona.Frontend.Redux.TestKit/Shared/States/SharedStateMock.cs
@@ -0,0 +1,17 @@
+﻿using Fluxor;
+using Moq;
+using ProjectHermes.Xipona.Frontend.Redux.Shared.States;
+
+namespace ProjectHermes.Xipona.Frontend.Redux.TestKit.Shared.States;
+
+public class SharedStateMock : Mock<IState<SharedState>>
+{
+    public SharedStateMock(MockBehavior behavior) : base(behavior)
+    {
+    }
+
+    public void SetupValue(SharedState returnValue)
+    {
+        Setup(m => m.Value).Returns(returnValue);
+    }
+}
---
+++ Frontend/Xipona.Frontend.Redux.Tests/Shared/Effects/SettingsEffectsFixtureBase.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using ProjectHermes.Xipona.Frontend.Redux.Shared.States;
+using ProjectHermes.Xipona.Frontend.Redux.TestKit.Common;
+using ProjectHermes.Xipona.Frontend.Redux.TestKit.Shared.Ports;
+using ProjectHermes.Xipona.Frontend.Redux.TestKit.Shared.States;
+
+namespace ProjectHermes.Xipona.Frontend.Redux.Tests.Shared.Effects;
+
+public class SettingsEffectsFixtureBase : EffectsFixtureBase
+{
+    protected readonly SharedStateMock SharedStateMock = new(MockBehavior.Strict);
+    protected readonly ShoppingListNotificationServiceMock NotificationServiceMock = new(MockBehavior.Strict);
+    protected SharedState State = new DomainTestBuilder<SharedState>().Create();
+
+    public void SetupStateReturningState()
+    {
+        SharedStateMock.SetupValue(State);
+    }
+}
